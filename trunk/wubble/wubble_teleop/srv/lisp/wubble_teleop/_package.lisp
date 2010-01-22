04002f186b3d9a8960f7d3d583020072f113b8c1
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-01-22 00:59:57 +0000

    Trying to remove some unwanted folders from the repo

--- trunk/wubble/wubble_teleop/srv/lisp/wubble_teleop/_package.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage wubble_teleop-srv
-  (:use cl
-        roslisp-msg-protocol)
-  (:export
-   "TARGETPOSITION"
-   "<TARGETPOSITION-REQUEST>"
-   "<TARGETPOSITION-RESPONSE>"
-  ))
-
--- trunk/wubble/wubble_teleop/srv/lisp/wubble_teleop/_package_TargetPosition.lisp
+++
@@ -1,6 +0,0 @@
-(in-package wubble_teleop-srv)
-(export '(X-VAL
-          Y-VAL
-          Z-VAL
-          SUCCESS-VAL
-))
