95e88146bb0cf60a2962e719324f685bc02743f6
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-01-23 05:47:53 +0000

    Finish arm driver, can now send new angles to joints and receive feedback.

---
+++ arrg/drivers/wubble_controllers/src/wubble_controllers/arm_ax12_const.py
@@ -0,0 +1,11 @@
+"""
+Arm Joint Name Constants
+"""
+
+SHOULDER_PAN = 'shoulder_pan_joint'
+SHOULDER_TILT = 'shoulder_tilt_joint'
+ELBOW_TILT = 'elbow_tilt_joint'
+WRIST_ROTATE = 'wrist_rotate_joint'
+FINGER_RIGHT = 'finger_right_joint'
+FINGER_LEFT = 'finger_left_joint'
+
