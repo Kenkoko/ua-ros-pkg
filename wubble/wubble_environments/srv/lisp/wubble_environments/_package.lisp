e395856b759360ae0c057a8925ec7c736ddfdfe9
anhxuan.tran <anhxuan.tran@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-01-26 21:20:32 +0000

    Removed and ignored auto-generated files (i.e. build, bin, etc.).

--- wubble/wubble_environments/srv/lisp/wubble_environments/_package.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage wubble_environments-srv
-  (:use cl
-        roslisp-msg-protocol)
-  (:export
-   "ICARUSWORLDSTATE"
-   "<ICARUSWORLDSTATE-REQUEST>"
-   "<ICARUSWORLDSTATE-RESPONSE>"
-  ))
-
