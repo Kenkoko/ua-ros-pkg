bd2ddf846083c964a27fa17da0041466d96ad419
anhxuan.tran <anhxuan.tran@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-02-15 23:49:57 +0000

    Fixed wubble_head_action, also added a simple client to test the head_action.

---
+++ trunk/arrg/ua_controllers/wubble_actions/src/move_head_client.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2010, Arizona Robotics Research Group, University of Arizona
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Willow Garage, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+PKG = 'wubble_actions'
+NAME = 'move_head_client'
+
+import roslib; roslib.load_manifest(PKG)
+import rospy
+
+from actionlib import SimpleActionClient
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Point
+from wubble_actions.msg import *
+
+
+def look_at(frame_id, x, y, z):
+
+    # Creates a goal to send to the action server.
+    goal = WubblePointHeadGoal()
+    goal.point.x = x
+    goal.point.y = y
+    goal.point.z = z
+    goal.frame_id = frame_id
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_goal_to_finish()
+
+    # Return result
+    return client.get_result()
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node(NAME, anonymous=True)
+        client = SimpleActionClient("wubble_point_head_action", WubblePointHeadAction)
+        client.wait_for_server()
+
+        # Looks at a point forward (x=5m), slightly left, and up
+        result = look_at("/base_footprint", 5.0, 6.0, 3.5);
+        print "Result: [" + str(result.head_position[0]) + ", " + str(result.head_position[1]) + "]"
+
+        # Looks at a point forward (x=5m), slightly right, and down
+        result = look_at("/base_footprint", 5.0, -6.0, 0.5);
+        print "Result: [" + str(result.head_position[0]) + ", " + str(result.head_position[1]) + "]"
+
+    except rospy.ROSInterruptException:
+        pass
+
