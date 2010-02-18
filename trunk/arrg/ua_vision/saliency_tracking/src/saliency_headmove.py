2f249585255c3440c21399e915cd5f25f099ea1f
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-02-18 22:42:10 +0000

    Make head center on most salient point.

--- trunk/arrg/ua_vision/saliency_tracking/src/saliency_headmove.py
+++ trunk/arrg/ua_vision/saliency_tracking/src/saliency_headmove.py
@@ -2,7 +2,7 @@
 #
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009, Arizona Robotics Research Group,
+# Copyright (c) 2010, Arizona Robotics Research Group,
 # University of Arizona. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,54 +32,55 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
+# Author: Antons Rebguns
 
 import roslib
-roslib.load_manifest('saliency_track')
+roslib.load_manifest('saliency_tracking')
 
 import rospy
+import math
 from geometry_msgs.msg import Point
-from wubble_controllers.msg import JointStateList
-from wubble_controllers.msg import PanTilt
+from pr2_controllers_msgs.msg import JointControllerState
+from std_msgs.msg import Float64
 
 class TrackSaliencyPOI():
     def __init__(self):
         self.hfov = 49.2
         self.vfov = 36.0
         self.center = Point(320, 240, 0)
-        self.deg_pan_ratio = hfov / 640.0      # how many degrees in a pixel in x plane
-        self.deg_tilt_ratio = vfov / 480.0       # how many degrees in a pixel in y plane
-        self.pan_deg_ratio = 640.0 / hfov  # how many pixels in a degree in x plane
-        self.tilt_deg_ratio = 480.0 / vfov  # how many pixels in a degree in y plane
+        self.deg_pan_ratio = self.hfov / 640.0       # how many degrees in a pixel in x plane
+        self.deg_tilt_ratio = self.vfov / 480.0      # how many degrees in a pixel in y plane
+        self.pan_deg_ratio = 640.0 / self.hfov       # how many pixels in a degree in x plane
+        self.tilt_deg_ratio = 480.0 / self.vfov      # how many pixels in a degree in y plane
         
         rospy.init_node('saliency_track_poi', anonymous=False)
         self.saliency_poi_sub = rospy.Subscriber('saliency_poi', Point, self.do_track_poi)
-        self.move_head_pub = rospy.Publisher('camera_pan_tilt_controller/pan_tilt', PanTilt)
-        self.joint_state_sub = rospy.Subscriber('camera_pan_tilt_controller/state', JointStateList, self.process_current_state)
+        self.move_head_pan_pub = rospy.Publisher('head_pan_controller/command', Float64)
+        self.move_head_tilt_pub = rospy.Publisher('head_tilt_controller/command', Float64)
+        self.joint_state_sub = rospy.Subscriber('head_pan_controller/state', JointControllerState, self.process_current_pan_state)
+        self.joint_state_sub = rospy.Subscriber('head_tilt_controller/state', JointControllerState, self.process_current_tilt_state)
         
         self.current_pan_angle = 0
         self.current_tilt_angle = 0
         self.current_pan_moving = False
         self.current_tilt_moving = False
 
-    def process_current_state(self, state_list):
-        pan_state = state_list.motor_states[0]
-        tilt_state = state_list.motor_states[1]
-        self.current_pan_angle = pan_state.angle
-        self.current_tilt_angle = tilt_state.angle
-        self.current_pan_moving = pan_state.moving
-        self.current_tilt_moving = tilt_state.moving
+    def process_current_pan_state(self, new_state):
+        self.current_pan_angle = math.degrees(new_state.process_value)
+
+    def process_current_tilt_state(self, new_state):
+        self.current_tilt_angle = math.degrees(new_state.process_value)
 
     def do_track_poi(self, point):
         x = point.x
         y = point.y
-        x_dist = x - self.center.x
+        x_dist = -x + self.center.x
         y_dist = -y + self.center.y
         x_dist_ang = x_dist * self.deg_pan_ratio
         y_dist_ang = y_dist * self.deg_tilt_ratio
 
-        if not self.current_pan_moving and not self.current_tilt_moving:
-#            if x_dist > 150 or y_dist > 70:
-            self.move_head_pub.publish(self.current_pan_angle + x_dist_ang, self.current_tilt_angle + y_dist_ang)
+        self.move_head_pan_pub.publish(math.radians(self.current_pan_angle + x_dist_ang))
+        self.move_head_tilt_pub.publish(math.radians(self.current_tilt_angle + y_dist_ang))
         
 if __name__ == '__main__':
     try:
