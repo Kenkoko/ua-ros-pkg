2717b2ef55818ad50937d3bbcd3e92e65eea3ec3
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-03-17 04:01:52 +0000

    Better graph generation and laser data recording in path_learning

--- trunk/arrg/ua_experimental/path_learning_experiment/nodes/simple_laser_filter.py
+++ trunk/arrg/ua_experimental/path_learning_experiment/nodes/simple_laser_filter.py
@@ -1,61 +1,42 @@
 #!/usr/bin/env python
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the Willow Garage, Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 # Author: Daniel Hewlett
 
 PKG = 'path_learning_experiment'
 
 import roslib; roslib.load_manifest(PKG)
-
-import time
-from threading import Thread
-
 import rospy
+
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
+from path_learning_experiment.msg import DistanceInfo
+
+import time
+import numpy
 
 class SimpleLaserFilter():
     def __init__(self):
-        self.distance_pub = rospy.Publisher('front_distance', Float64)
-        self.average_pub = rospy.Publisher('average_distance', Float64)
+        #self.distance_pub = rospy.Publisher('front_dist', Float64)
+        #self.average_pub = rospy.Publisher('avg_dist', Float64)
+        #self.min_pub = rospy.Publisher('min_dist', Float64)
+        self.dist_pub = rospy.Publisher('dist_info', DistanceInfo)        
+
         rospy.init_node('simple_laser_filter', anonymous=True)
+
         rospy.Subscriber('tilt_scan', LaserScan, self.filter_scan)
 
     def filter_scan(self, scan):
+        di = DistanceInfo()
+        di.header = scan.header    
+        di.min_dist = numpy.min(scan.ranges)
         num_scans = len(scan.ranges)
         center_scan_index = (num_scans + 1) / 2
         center_range = scan.ranges[center_scan_index]
-        self.distance_pub.publish(center_range)
+        di.front_dist = center_range
         sum_dist = 0        
         for dist in scan.ranges:
             sum_dist += dist
-        avg_dist = sum_dist / num_scans 
-        self.average_pub.publish(avg_dist)
+        di.avg_dist = sum_dist / num_scans 
+        self.dist_pub.publish(di)
 
 if __name__ == '__main__':
     try:
