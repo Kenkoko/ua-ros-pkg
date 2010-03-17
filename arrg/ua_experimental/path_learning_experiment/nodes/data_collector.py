48cda55d7e1edbf7f51f645b192dbcc05a7d364c
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-03-17 04:01:52 +0000

    Better graph generation and laser data recording in path_learning

--- arrg/ua_experimental/path_learning_experiment/nodes/data_collector.py
+++ arrg/ua_experimental/path_learning_experiment/nodes/data_collector.py
@@ -1,53 +1,31 @@
 #!/usr/bin/env python
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the Willow Garage, Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 # Author: Daniel Hewlett
 
 PKG = 'path_learning_experiment'
 
 import roslib; roslib.load_manifest(PKG)
+import rospy
 import actionlib
 
-import rospy
 from std_msgs.msg import Float64
 from geometry_msgs.msg import PoseStamped, Pose, Point
 from move_base_msgs.msg import MoveBaseActionFeedback
 from actionlib_msgs.msg import GoalStatusArray
 from nav_msgs.msg import Path
+from path_learning_experiment.msg import DistanceInfo
 
 import random
-import time
 import math
 
 import matplotlib.pyplot as plt
 
+def dist(x1, y1, x2, y2):
+       return math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2))
+
+
+
 class Episode():
-    def __init__(self, plan_waypoints, actual_waypoints):
+    def __init__(self, plan_waypoints, actual_waypoints, distances):
         #self.goal_x = goal_pos.position.x
         #self.goal_y = goal_pos.position.y
         #self.start_x = start_pos.position.x
@@ -59,25 +37,55 @@ class Episode():
             self.planned_x += [waypoint.position.x]
             self.planned_y += [waypoint.position.y]
 
+        # Hack, this should really come from the actual goal 
+        self.goal_x = self.planned_x[-1]
+        self.goal_y = self.planned_y[-1]
+
+        self.dist_t = []
+        self.dist_value = []
+        self.start_time = actual_waypoints[0].header.stamp
+        self.end_time = actual_waypoints[-1].header.stamp
+
         self.current_x = []
         self.current_y = []
         for waypoint in actual_waypoints:
-            self.current_x += [waypoint.position.x]
-            self.current_y += [waypoint.position.y]    
+            self.current_x += [waypoint.pose.position.x]
+            self.current_y += [waypoint.pose.position.y]    
+            self.dist_t += [(waypoint.header.stamp - self.start_time).to_sec()]
+            self.dist_value += [dist(self.current_x[-1], self.current_y[-1], self.goal_x, self.goal_y)]
+
+        self.scan_t = []
+        self.scan_min = []
+        for d in distances:
+            if d.header.stamp >= self.start_time and d.header.stamp <= self.end_time:
+                self.scan_t += [(d.header.stamp - self.start_time).to_sec()]
+                self.scan_min += [d.min_dist]
 
     def plot(self):
-        plt.clf()        
-        plt.plot(self.current_x, self.current_y, 'go')
-        plt.plot(self.planned_x, self.planned_y, 'bs')
+        t = rospy.Time.now()
+
+        plt.clf()  
+        plt.subplot(211)      
+        plt.plot(self.planned_x, self.planned_y, 'bo')        
+        plt.plot(self.current_x, self.current_y, 'gv')       
         plt.axis([-3.5,3.5,-2.5,2.5])
-        plt.draw()
-        plt.savefig(str(rospy.Time.now()) + '.png')
-        #plt.show()
+#        plt.draw()
+#        plt.savefig(str(t) + '-image.png')
         
+#        plt.clf()
+        plt.subplot(212)
+        plt.plot(self.dist_t, self.dist_value)
+        plt.plot(self.scan_t, self.scan_min, 'r')
+        
+        plt.draw()
+        plt.savefig(str(t) + '-plot.png')
+
+        #plt.show()
+      
+  
                
 class DataCollector():
     def __init__(self):
-        self.is_running = True
         self.x_range = (-3.5,3.5)
         self.y_range = (-2.5,2.5)
         self.previous_success = True   
@@ -92,6 +100,8 @@ class DataCollector():
         self.actual_waypoints = []
 
         self.old_status = None
+
+        self.distances = []
     
         rospy.init_node('data_collector', anonymous=True)
     
@@ -100,16 +110,15 @@ class DataCollector():
         rospy.Subscriber('move_base/status', GoalStatusArray, self.handle_status)
         rospy.Subscriber('move_base_node/NavfnROS/plan', Path, self.handle_plan)        
 
+        rospy.Subscriber('dist_info', DistanceInfo, self.handle_dist)
+    
     def save_episode(self):
-        e = Episode(self.plan_waypoints, self.actual_waypoints)
+        e = Episode(self.plan_waypoints, self.actual_waypoints, self.distances)
         self.episodes += [e]
         self.plan_waypoints = []
         self.actual_waypoints = []
+        self.distances = []
         e.plot()
-
-    def main_loop(self):
-        while self.is_running:
-            time.sleep(0.5)
 
     def handle_goal(self, msg):
         self.goal_pos = msg.pose
@@ -117,27 +126,23 @@ class DataCollector():
     def handle_status(self, msg):
         if msg.status_list:
             curr_status = msg.status_list[0].status
-            print curr_status
-            #if curr_status == 1:
-            #    self.current_x = 
         
     def handle_feedback(self, msg):
-        self.actual_waypoints += [msg.feedback.base_position.pose]
+        self.actual_waypoints += [msg.feedback.base_position]
 
     def handle_plan(self, msg):
         if self.plan_waypoints:        
             self.save_episode()
-
         for pose_stamped in msg.poses:
             self.plan_waypoints += [pose_stamped.pose]
+    
+    def handle_dist(self, msg):
+        self.distances += [msg]
+
 
 if __name__ == '__main__':
     try:
         collector = DataCollector()
-        #t = Thread(target=collector.main_loop)
-        #t.start()
         rospy.spin()
-        collector.is_running = False
-        #t.join()
     except rospy.ROSInterruptException: pass
 
