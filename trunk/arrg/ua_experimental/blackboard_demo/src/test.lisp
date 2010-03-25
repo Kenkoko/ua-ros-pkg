4c1604e08d42964f1e870b61b658cad00ff595fb
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-03-25 00:00:13 +0000

    More fixes to make translating message classes more automatic

--- trunk/arrg/ua_experimental/blackboard_demo/src/test.lisp
+++ trunk/arrg/ua_experimental/blackboard_demo/src/test.lisp
@@ -1,46 +1,15 @@
 (in-package :blackboard_demo)
 
+;(translate-unit-class (find-class 'gazebo_plugins-msg:<WorldState>)
+;(message-type-to-unit-class "gazebo_plugins" "WorldState")
+
+(defparameter *msg* nil)
+
 ;; Initial translation of the WorldState message class into a GBBopen class
 (defun test ()
   (with-ros-node ("test" :spin t)
-    (subscribe "gazebo_world_state" "gazebo_plugins/WorldState" #'update-world-state)))
-
-(defun get-simple-name (sym)
-  (read-from-string (symbol-name sym)))
-
-(defun collect-slots (class-name)
-  (loop for slot-def in (gbbopen-tools:class-direct-slots (find-class class-name))
-     collect (read-from-string (symbol-name (gbbopen-tools:slot-definition-name slot-def)))))
-
-(defmacro translate-unit-class (class-name)
-  `(define-unit-class ,(intern (symbol-name class-name)) ()
-        ,(collect-slots class-name)))
-
-;; Super hacky, need to probably store the symbols that name the generated
-;; classes in a hash table or something, appending "blackboard_demo" is not very general
-(defun translate-msg (msg)
-  (apply #'make-instance 
-         (read-from-string (concatenate 'string 
-                                        "blackboard_demo::" 
-                                        (symbol-name (class-name (class-of msg)))))
-         (loop for slot-def in (gbbopen-tools:class-direct-slots (class-of msg))
-            for slot-name = (gbbopen-tools:slot-definition-name slot-def)
-            append (list (intern (symbol-name slot-name) 'keyword)
-                         (slot-value msg slot-name)))))
+    (subscribe "gazebo_world_state" "gazebo_plugins/WorldState" #'translate-msg)))
 
 (defun update-world-state (state-msg)
   (let* ((ui (translate-msg state-msg)))
-    (describe-instance ui)))
-
-(defun object-test ()
-  (let* ((ui (make-instance 'object 
-                            :name "red_cube_1"
-                            :color 'red
-                            :shape 'cube
-                            :location (make-instance 'position-3d
-                                                     :x 0.5
-                                                     :y 1.5
-                                                     :z 2.0))))
-    (add-instance-to-space-instance ui *object-space*)
-    (describe-blackboard-repository)
     (describe-instance ui)))
