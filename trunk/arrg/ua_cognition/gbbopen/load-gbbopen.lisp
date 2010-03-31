123237975c4b1ef20ad71fb844263e685b99dc52
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-03-31 22:42:43 +0000

    Fixes to gbbopen package to make agenda-shell functionality easier to use, as well as clean up glitches in asd file and initial compilation

--- trunk/arrg/ua_cognition/gbbopen/load-gbbopen.lisp
+++ trunk/arrg/ua_cognition/gbbopen/load-gbbopen.lisp
@@ -1,3 +1,4 @@
 (load "gbbopen/startup.lisp")
 (module-manager:compile-module :gbbopen-user :propagate :create-dirs)
+(module-manager:compile-module :agenda-shell-user :propagate :create-dirs)
 (quit)
