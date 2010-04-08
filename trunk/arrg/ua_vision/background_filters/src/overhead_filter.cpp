c0b6097b85ded42e4b7208e527e3c1a5290987bb
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-04-08 04:05:17 +0000

    Added contour detection, tight and aligned bounding boxes, and preliminary object list message.

--- trunk/arrg/ua_vision/background_filters/src/overhead_filter.cpp
+++ trunk/arrg/ua_vision/background_filters/src/overhead_filter.cpp
@@ -3,7 +3,9 @@
 
 #include <ros/ros.h>
 #include "sensor_msgs/Image.h"
+#include "sensor_msgs/RegionOfInterest.h"
 #include "cv_bridge/CvBridge.h"
+#include "background_filters/ObjectList.h"
 
 #include <cv.h>
 #include <cxcore.h>
@@ -13,12 +15,36 @@ using namespace std;
 
 IplImage* bg = NULL; 
 IplImage *original, *curr_image, *mono_image;
+CvMemStorage *storage;
+CvSeq* contour;
 
 bool isFirst = true;
 
 sensor_msgs::CvBridge bridge;
 
-ros::Publisher sub_pub, mask_pub;
+ros::Publisher sub_pub, mask_pub, object_pub;
+
+static void draw_box(IplImage *image, CvBox2D box, double color) {
+  CvPoint2D32f boxPoints[4];
+
+  cvBoxPoints(box, boxPoints);
+  cvLineAA(image,
+	   cvPoint((int)boxPoints[0].x, (int)boxPoints[0].y),
+	   cvPoint((int)boxPoints[1].x, (int)boxPoints[1].y),
+	   color);
+  cvLineAA(image,
+	   cvPoint((int)boxPoints[1].x, (int)boxPoints[1].y),
+	   cvPoint((int)boxPoints[2].x, (int)boxPoints[2].y),
+	   color);
+  cvLineAA(image,
+	   cvPoint((int)boxPoints[2].x, (int)boxPoints[2].y),
+	   cvPoint((int)boxPoints[3].x, (int)boxPoints[3].y),
+	   color);
+  cvLineAA(image,
+	   cvPoint((int)boxPoints[3].x, (int)boxPoints[3].y),
+	   cvPoint((int)boxPoints[0].x, (int)boxPoints[0].y),
+	   color);
+}
 
 void handleImage(const sensor_msgs::ImageConstPtr& msg_ptr) 
 {
@@ -29,6 +55,8 @@ void handleImage(const sensor_msgs::ImageConstPtr& msg_ptr)
 	    if (isFirst) {
 		    curr_image = cvCreateImage(cvGetSize(original), 8, 3);
 		    mono_image = cvCreateImage(cvGetSize(original), 8, 1);
+            storage  = cvCreateMemStorage(0);
+            contour = 0;
 		    isFirst = false;
 	    }
 		
@@ -40,8 +68,50 @@ void handleImage(const sensor_msgs::ImageConstPtr& msg_ptr)
 				
 		cvCopy(original, curr_image, mono_image);
 		
+		cvFindContours(mono_image, storage, &contour, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+		
+		background_filters::ObjectList objectList;
+		
+		//cout << "HERE" << endl << endl;
+		for( ; contour != 0; contour = contour->h_next )
+		{
+			CvScalar color = CV_RGB( rand()&255, rand()&255, rand()&255 );
+			double area = fabs(cvContourArea(contour));
+			//cout << area << endl;
+			if (area > 200) 
+			{
+			    cvDrawContours(original, contour, color, color, -1, 2, 8 );
+			    
+			    CvBox2D minBox = cvMinAreaRect2(contour, storage);
+			    draw_box(original, minBox, 1.0);
+			    
+			    CvRect boundingBox = cvBoundingRect(contour);
+			    CvPoint topLeft, bottomRight;
+			    topLeft.x = boundingBox.x;
+			    topLeft.y = boundingBox.y;
+			    bottomRight.x = boundingBox.x + boundingBox.width;
+			    bottomRight.y = boundingBox.y + boundingBox.height;
+			    cvRectangle(original, topLeft, bottomRight, color);
+			    
+			    sensor_msgs::RegionOfInterest roi;
+			    roi.x_offset = boundingBox.x;
+			    roi.y_offset = boundingBox.y;
+			    roi.width = boundingBox.width;
+			    roi.height = boundingBox.height;
+			    
+			    objectList.objectRegions.push_back(roi);
+		
+		      	// TODO: I don't know exactly how to make the sub images based on the 
+            	// bounding box. I'm pretty sure it should use cvGetSubRect.        	    
+			    //cvGetSubRect(original
+		    }
+        }	
+	
+		//objectList.originalImage = *bridge.cvToImgMsg(original, "rgb8");
+		
+		object_pub.publish(objectList);
 		mask_pub.publish(bridge.cvToImgMsg(mono_image, "mono8"));
-		sub_pub.publish(bridge.cvToImgMsg(curr_image, "rgb8"));
+		sub_pub.publish(bridge.cvToImgMsg(original, "rgb8"));
 	}
 	catch (sensor_msgs::CvBridgeException error)
 	{
@@ -57,8 +127,9 @@ int main (int argc, char **argv)
     
     ros::NodeHandle n;
 
-    sub_pub = n.advertise<sensor_msgs::Image>("/sub_image", 20);
-    mask_pub = n.advertise<sensor_msgs::Image>("/mask_image", 20);
+    sub_pub = n.advertise<sensor_msgs::Image>("sub_image", 20);
+    mask_pub = n.advertise<sensor_msgs::Image>("mask_image", 20);
+    object_pub = n.advertise<background_filters::ObjectList>("object_list", 20);
 
     ros::Subscriber image_sub = n.subscribe("/camera/image_color", 20, handleImage);
     
