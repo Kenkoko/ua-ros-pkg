cac1714481165e933f59bc2be0aa350804143b95
ianfasel <ianfasel@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-04-19 02:00:27 +0000

    Added round robin tournament scheduling to game_master

--- trunk/arrg/ua_game_theory/game_faces/src/game_master.py
+++ trunk/arrg/ua_game_theory/game_faces/src/game_master.py
@@ -3,6 +3,7 @@ import roslib; roslib.load_manifest('game_faces')
 from game_faces.srv import *
 from game_faces.msg import TwoPersonGame
 import rospy
+import numpy
 
 num_registered_players = 0
 num_players = 0
@@ -20,8 +21,41 @@ def register_game_player(req):
         print "We would return"
         registration_service.shutdown()
     return RegisterGamePlayerResponse(num_registered_players)
-            
 
+# Below round robin code from
+## {{{ http://code.activestate.com/recipes/65200/ (r1)
+def roundRobin(units, sets=None):
+    """ Generates a schedule of "fair" pairings from a list of units """
+    if len(units) % 2:
+        units.append(None)
+    count    = len(units)
+    sets     = sets or (count - 1)
+    half     = count / 2
+    schedule = []
+    for turn in range(sets):
+        pairings = []
+        for i in range(half):
+            pairings.append((units[i], units[count-i-1]))
+        units.insert(1, units.pop())
+        schedule.append(pairings)
+    return schedule
+ 
+def create_block(num_players, do_twice):
+    pairs = roundRobin(range(num_players))
+    if do_twice:
+        pc = pairs[:]
+        for p in pc:
+            rp = []
+            for game in p:
+                g = list(game)
+                g.reverse()
+                rp.append(tuple(g))
+            pairs.append(rp)
+        
+    for pairings in pairs:
+        print pairings
+    
+    return pairs
 
 def game_master_start():
     global num_registered_players
@@ -54,22 +88,30 @@ def game_master_start():
     #  Number of experimental blocks
     #  Desired number of times a pair occurs
 
-    num_blocks = 1
-    for i in range(0,num_blocks):
-        #pairing_sets = get_random_pairings_for_block(num_players)
-        pairing_sets = [[1,2]]
-        game_topic = 0
-        print "pairing sets: " + str(pairing_sets)
-        for pair in pairing_sets:
-            print "pair: " + str(pair)
-            game_topic+=1
-            try:
-                game_type = "TrustGame"
-                msg = TwoPersonGame('game_topic'+str(game_topic), pair[0], pair[1], game_type)                
-                print 'game_topic'+str(game_topic)
-                gameTopic.publish(msg)
-            except rospy.ROSInterruptException, e:
-                print "something terrible happened"
+    num_blocks = 3
+    for i in range(num_blocks):
+        if i == 0:
+            pairs = create_block(num_players, True):
+            game_type = "TrustGame"
+        if i == 1:
+            pairs = create_block(num_players, False):
+            game_type = "Prisoners"
+        if i == 2:
+            pairs = create_block(num_players, True):
+            game_type = "Ultimatum"
+
+        for block_round in pairs
+            game_topic = 0
+            for pair in block_round:
+                print "pair: " + str(pair)
+                game_topic+=1
+                try:
+                    msg = TwoPersonGame('game_topic'+str(game_topic), pair[0], pair[1], game_type)                
+                    print 'game_topic'+str(game_topic)
+                    gameTopic.publish(msg)
+                except rospy.ROSInterruptException, e:
+                    print "something terrible happened"
+            # Some logic here to wait until all the games finish playing
 
         
 import sys
