bfa627aafb3419a31b7b8501864f15a0af702483
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-05-10 05:55:39 +0000

    Code related to running simulators with a simple robot base instead of just primitives

--- arrg/ua_experimental/simulator_experiments/nodes/test.py
+++ arrg/ua_experimental/simulator_experiments/nodes/test.py
@@ -21,10 +21,10 @@ def spawn_model_client(name, xml):
 
         sp.model.robot_namespace = '/'
         
-        sp.model.initial_pose.position.x = 0
-        sp.model.initial_pose.position.y = 0
+        sp.model.initial_pose.position.x = 0.0
+        sp.model.initial_pose.position.y = 0.0
         sp.model.initial_pose.position.z = 0.3
-        sp.model.initial_pose.orientation.w = 1
+        sp.model.initial_pose.orientation.w = 1.0
 
         resp1 = spawn_model(sp)
         print resp1.success
@@ -35,12 +35,16 @@ def spawn_model_client(name, xml):
 count = 0
 def spawn_model(file_name):
     global count
-    xml_file = open(roslib.packages.find_resource("simulator_experiments", file_name)[0])
+    #xml_file = open(roslib.packages.find_resource("simulator_experiments", file_name)[0])
+    xml_file = open(roslib.packages.find_resource("gazebo", file_name)[0])
     gazebo_xml = xml_file.read()
     count += 1
-    spawn_model_client('test_object' + str(count), gazebo_xml)
+    spawn_model_client('test_object2' + str(count), gazebo_xml)
 
 if __name__ == "__main__":
-    spawn_model("blue_sphere.xml")
-    time.sleep(3)
-    spawn_model("blue_box.xml")
+    spawn_model("simplecar.model")
+    #spawn_model("blue_sphere.xml")
+    #time.sleep(3)
+    #spawn_model("blue_box.xml")
+    #spawn_model("pr2.model")
+    
