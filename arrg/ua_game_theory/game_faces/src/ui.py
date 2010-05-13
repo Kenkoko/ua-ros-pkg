4cf96978bfa491cfa49a58092691f5b2943ef51e
Cody.D.Jorgensen <Cody.D.Jorgensen@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-05-13 22:06:14 +0000

    making progress on game ui

--- arrg/ua_game_theory/game_faces/src/ui.py
+++ arrg/ua_game_theory/game_faces/src/ui.py
@@ -1,188 +1,275 @@
 #!/usr/bin/env python
 
-# example spinbutton.py
+import sys
+import time
+import socket
+import threading
 
 import pygtk
 pygtk.require('2.0')
 import gtk
 
-class SpinButtonExample:
-    def toggle_snap(self, widget, spin):
-        spin.set_snap_to_ticks(widget.get_active())
+import roslib
+roslib.load_manifest('game_faces')
+import rospy
 
-    def toggle_numeric(self, widget, spin):
-        spin.set_numeric(widget.get_active())
+from game_faces.srv import *
+from game_faces.msg import TwoPersonGame, GamePlay
 
-    def change_digits(self, widget, spin, spin1):
-        spin1.set_digits(spin.get_value_as_int())
+gtk.gdk.threads_init()
 
-    def get_value(self, widget, data, spin, spin2, label):
-        if data == 1:
-            buf = "%d" % spin.get_value_as_int()
-        else:
-            buf = "%0.*f" % (spin2.get_value_as_int(),
-                             spin.get_value())
-        label.set_text(buf)
+class GtkThreadSafe:
+    def __enter__(self):
+        gtk.gdk.threads_enter()
+    
+    def __exit__(self, _type, value, traceback):
+        # do any error handling, return False to propogate
+        # error, True if handled here
+        gtk.gdk.threads_leave()
 
-    def __init__(self):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.connect("destroy", lambda w: gtk.main_quit())
-        window.set_title("Game")
-        #window.resize(400, 300)
+class RosTopicProxy:
+    def __init__(self, topic, msg_type, callback):
+        self.sub = None
+        self.topic = topic
+        self.msg_type = msg_type
+        self.callback = callback
+    
+    def connect(self):
+        if not rospy.core.is_initialized():
+            # only called once per execution
+            try:
+                print roslib.scriptutil.get_master().getPid('/')
+            except socket.error:
+                print "Unable to communicate with master!"
+            else:
+                rospy.init_node('ui_topic_proxy', anonymous=True)
+        self.sub = rospy.Subscriber(self.topic, self.msg_type, self.callback)
+    
+    def disconnect(self):
+        if self.sub:
+            self.sub.unregister()
+    
+    def __del__(self):
+        self.disconnect()
 
-        main_vbox = gtk.VBox(False, 8)
-        main_vbox.set_border_width(10)
-        window.add(main_vbox)
+class GFPlayer:
+    def __init__(self, player_id, is_first, game_topic):
+        self.player_id = player_id
+        self.is_first = is_first
+        self.game_topic = game_topic
+        self.game_topic_lock = threading.Lock()
+
+class UltimatumGame:
+    def __init__(self, shared_console, player):
+        self.shared_console = shared_console
+        self.player = player
+        
+        self.enabled = True
+        self.play_pub = rospy.Publisher(self.player.game_topic, GamePlay, subscriber_listener=None, tcp_nodelay=True, latch=(not self.player.is_first))
+        self.play_sub = rospy.Subscriber(self.player.game_topic, GamePlay, self.take_turn)
+        
+        self.title = "Ultimatum"
+        self.frame = gtk.Frame(self.title)
+        game_vbox = gtk.VBox(False, 8)
+        self.frame.add(game_vbox)
         
         self.box_status = gtk.HBox(False, 8)
-        main_vbox.add(self.box_status)
+        game_vbox.pack_start(self.box_status, expand=False, fill=False)
         
-        self.current_balance = gtk.Label('Your current balance is ...')
-        self.current_balance.set_alignment(0, 0.5)
-        self.box_status.add(self.current_balance)
-
+        self.label_balance_desc = gtk.Label('Your current balance is')
+        self.box_status.add(self.label_balance_desc)
+        
+        self.label_balance_amt = gtk.Label()
+        self.set_balance(0)
+        self.box_status.add(self.label_balance_amt)
 
         self.box_control = gtk.HBox(False, 8)
-        #main_vbox.pack_start(self.box_control, True, True, 0)
-        main_vbox.add(self.box_control)
+        game_vbox.pack_start(self.box_control, expand=False, fill=False)
         
-        self.box_control.add(gtk.Label('Your bid'))
+        self.box_control.add(gtk.Label('Amount to give'))
         
         self.spin_bid = gtk.SpinButton(adjustment=gtk.Adjustment(lower=1, upper=10, step_incr=1))
         self.box_control.add(self.spin_bid)
         
         self.button_bid = gtk.Button(stock=gtk.STOCK_OK)
         self.button_bid.set_size_request(80, 30)
+        self.button_bid.connect("clicked", self.ok_button_clicked)
         self.box_control.add(self.button_bid)
         
-        text_buffer = gtk.TextBuffer()
-        text_buffer.insert(text_buffer.get_end_iter(), "test...\n")
-        text_buffer.insert(text_buffer.get_end_iter(), "more test\n")
-        self.textview_console = gtk.TextView(text_buffer)
-        self.textview_console.set_editable(False)
-        self.textview_console.set_size_request(400, 120)
-        main_vbox.add(self.textview_console)
+        self.frame.show_all()
+        self.toggle_user_interaction()
+    
+    def ok_button_clicked(self, widget, data=None):
+        self.shared_console.append_text('hello world\n')
+    
+    def set_balance(self, balance):
+        self.label_balance_amt.set_markup('<span size="20000" weight="bold">%d</span>' %balance)
+    
+    def toggle_user_interaction(self):
+        self.enabled = not self.enabled
+        self.box_control.set_sensitive(self.enabled)
+    
+    def take_first_turn(self):
+        if not self.player.is_first:
+            gp = GamePlay(play_number=0,amount=0, player_id=self.player.player_id)
+            gp.header.stamp = rospy.Time.now()
+            self.play_pub.publish(gp)
         
-  
-        # vbox = gtk.VBox(False, 0)
-        # vbox.set_border_width(5)
-        # frame.add(vbox)
-        # 
-        # # Day, month, year spinners
-        # hbox = gtk.HBox(False, 0)
-        # vbox.pack_start(hbox, True, True, 5)
-        #   
-        # vbox2 = gtk.VBox(False, 0)
-        # hbox.pack_start(vbox2, True, True, 5)
-        # 
-        # label = gtk.Label("Day :")
-        # label.set_alignment(0, 0.5)
-        # vbox2.pack_start(label, False, True, 0)
-        #   
-        # adj = gtk.Adjustment(1.0, 1.0, 31.0, 1.0, 5.0, 0.0)
-        # spinner = gtk.SpinButton(adj, 0, 0)
-        # spinner.set_wrap(True)
-        # vbox2.pack_start(spinner, False, True, 0)
-        #   
-        # vbox2 = gtk.VBox(False, 0)
-        # hbox.pack_start(vbox2, True, True, 5)
-        #   
-        # label = gtk.Label("Month :")
-        # label.set_alignment(0, 0.5)
-        # vbox2.pack_start(label, False, True, 0)
-        # 
-        # adj = gtk.Adjustment(1.0, 1.0, 12.0, 1.0, 5.0, 0.0)
-        # spinner = gtk.SpinButton(adj, 0, 0)
-        # spinner.set_wrap(True)
-        # vbox2.pack_start(spinner, False, True, 0)
-        #   
-        # vbox2 = gtk.VBox(False, 0)
-        # hbox.pack_start(vbox2, True, True, 5)
-        #   
-        # label = gtk.Label("Year :")
-        # label.set_alignment(0, 0.5)
-        # vbox2.pack_start(label, False, True, 0)
-        #   
-        # adj = gtk.Adjustment(1998.0, 0.0, 2100.0, 1.0, 100.0, 0.0)
-        # spinner = gtk.SpinButton(adj, 0, 0)
-        # spinner.set_wrap(False)
-        # spinner.set_size_request(55, -1)
-        # vbox2.pack_start(spinner, False, True, 0)
-        #   
-        # frame = gtk.Frame("Accelerated")
-        # main_vbox.pack_start(frame, True, True, 0)
-        #   
-        # vbox = gtk.VBox(False, 0)
-        # vbox.set_border_width(5)
-        # frame.add(vbox)
-        #   
-        # hbox = gtk.HBox(False, 0)
-        # vbox.pack_start(hbox, False, True, 5)
-        #   
-        # vbox2 = gtk.VBox(False, 0)
-        # hbox.pack_start(vbox2, True, True, 5)
-        #   
-        # label = gtk.Label("Value :")
-        # label.set_alignment(0, 0.5)
-        # vbox2.pack_start(label, False, True, 0)
-        #   
-        # adj = gtk.Adjustment(0.0, -10000.0, 10000.0, 0.5, 100.0, 0.0)
-        # spinner1 = gtk.SpinButton(adj, 1.0, 2)
-        # spinner1.set_wrap(True)
-        # spinner1.set_size_request(100, -1)
-        # vbox2.pack_start(spinner1, False, True, 0)
-        #   
-        # vbox2 = gtk.VBox(False, 0)
-        # hbox.pack_start(vbox2, True, True, 5)
-        #   
-        # label = gtk.Label("Digits :")
-        # label.set_alignment(0, 0.5)
-        # vbox2.pack_start(label, False, True, 0)
-        #   
-        # adj = gtk.Adjustment(2, 1, 5, 1, 1, 0)
-        # spinner2 = gtk.SpinButton(adj, 0.0, 0)
-        # spinner2.set_wrap(True)
-        # adj.connect("value_changed", self.change_digits, spinner2, spinner1)
-        # vbox2.pack_start(spinner2, False, True, 0)
-        #   
-        # hbox = gtk.HBox(False, 0)
-        # vbox.pack_start(hbox, False, True, 5)
-        # 
-        # button = gtk.CheckButton("Snap to 0.5-ticks")
-        # button.connect("clicked", self.toggle_snap, spinner1)
-        # vbox.pack_start(button, True, True, 0)
-        # button.set_active(True)
-        #   
-        # button = gtk.CheckButton("Numeric only input mode")
-        # button.connect("clicked", self.toggle_numeric, spinner1)
-        # vbox.pack_start(button, True, True, 0)
-        # button.set_active(True)
-        #   
-        # val_label = gtk.Label("")
-        #   
-        # hbox = gtk.HBox(False, 0)
-        # vbox.pack_start(hbox, False, True, 5)
-        # button = gtk.Button("Value as Int")
-        # button.connect("clicked", self.get_value, 1, spinner1, spinner2,
-        #                val_label)
-        # hbox.pack_start(button, True, True, 5)
-        #   
-        # button = gtk.Button("Value as Float")
-        # button.connect("clicked", self.get_value, 2, spinner1, spinner2,
-        #                val_label)
-        # hbox.pack_start(button, True, True, 5)
-        #   
-        # vbox.pack_start(val_label, True, True, 0)
-        # val_label.set_text("0")
-        #   
-        hbox = gtk.HBox(False, 0)
-        main_vbox.pack_start(hbox, False, True, 0)
-  
-        button = gtk.Button("Close")
-        button.connect("clicked", lambda w: gtk.main_quit())
-        hbox.pack_start(button, True, True, 5)
+    def take_turn(self, game_play):
+        gts = GtkThreadSafe()
+        play_number = game_play.play_number
+        if play_number == 0:
+            if self.player.is_first:
+                with gts:
+                    self.set_balance(10)
+                    self.toggle_user_interaction()
+                    self.shared_console.append_text("Choose an amount, between 0 and 10 and press OK\n")
+#                gp = GamePlay(play_number=1,amount=offer, player_id=self.player_id)
+#                gp.header.stamp = rospy.Time.now()
+#                self.play_pub.publish(gp)
+#                print "Waiting for second player"
+            else:
+                with gts:
+                    self.shared_console.append_text("Waiting for first player...\n")
+#        elif play_number == 1:
+#            if self.player.is_first_player:
+#                pass    
+#            else:
+#                print "Player 1 sent "+str(game_play.amount)
+#                print "You can either accept or reject this offer"
+#                choice = raw_input("Please, input 'A' to accept or 'R' to reject the offer")
+#                if choice == "A":
+#                    self.current_amount = str(game_play.amount)
+#                else:
+#				    self.current_amount = 0
+#                print "Your payoff is now: " + str(self.current_amount)
+#                gp = GamePlay(play_number=2,amount=ord(choice), player_id=self.player_id)
+#                gp.header.stamp = rospy.Time.now()
+#                self.play_pub.publish(gp)
+#        elif play_number == 2:
+#            if self.player.is_first_player:
+#                choice = chr(game_play.amount)
+#                if choice == 'A':
+#                    which_choice = "Accepted"
+#                    self.current_amount = self.original - self.offer
+#                else:
+#                    which_choice = "Rejected"
+#                    self.current_amount = 0
+#                print "The other player decide to" + str(which_choice) + "your offer"
+#                print "Your payoff for this round is:" + str(self.current_amount)
+#                gp = GamePlay(play_number=3,amount=-1, player_id=self.player_id)
+#                gp.header.stamp = rospy.Time.now(); self.play_pub.publish(gp)  
+#            else:
+#                pass
+#            print "Please wait for the next game to start"
+#            self.unregister_game()
+    
+    def unregister_game(self):
+        self.player.game_topic_lock.acquire()
+        self.play_pub.unregister()
+        self.play_sub.unregister()
+        self.player.game_topic = ""
+        self.player.game_topic_lock.release()
+
+class GFConsole:
+    def __init__(self):
+        self.frame = gtk.Frame()
+        self.text_buffer = gtk.TextBuffer()
+        self.textview = gtk.TextView(self.text_buffer)
+        self.textview.set_editable(False)
+        self.textview.set_cursor_visible(False)
+        self.scrollview = gtk.ScrolledWindow()
+        self.scrollview.add_with_viewport(self.textview)
+        self.scrollview.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.frame.add(self.scrollview)
+        self.frame.show_all()
+
+    def scroll_to_bottom(self):
+        v_adj = self.scrollview.get_vadjustment()
+        v_adj.set_value(v_adj.get_upper())
+    
+    def set_text(self, text):
+        self.text_buffer.set_text(str(text))
+        self.scroll_to_bottom()    
+        
+    def append_text(self, text):
+        self.text_buffer.insert(self.text_buffer.get_end_iter(), text)
+        self.scroll_to_bottom()
+        
+    def clear(self):
+        self.text_buffer.set_text('')
+
+class GameFacesUI:
+    def __init__(self):
+        # ivars
+        self.player = GFPlayer(-1, False, '')
+    
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.connect("destroy", lambda w: gtk.main_quit())
+        window.set_title("Game")
+        window.resize(500, 250)
+
+        self.content_vbox = gtk.VBox(False, 8)
+        self.content_vbox.set_border_width(10)
+        window.add(self.content_vbox)
+        
+        self.console = GFConsole()
+        self.content_vbox.pack_end(self.console.frame)
+        
         window.show_all()
+        self.register_player()
+        
+    def register_player(self):
+        self.console.append_text("Connecting to Game Master...\n")
+        rospy.init_node('game_faces_ui', anonymous=True)
+        rospy.Subscriber("game_master", TwoPersonGame, self.play_game)
+
+        rospy.wait_for_service('register_game_player')
+        try:
+            reg = rospy.ServiceProxy('register_game_player', RegisterGamePlayer)
+            response = reg(str(roslib.network.get_host_name()))
+            self.player.player_id = response.player_id
+        except rospy.ServiceException, e:
+            print "Could not get a player_id from master"
+            sys.exit(1)
+        
+        self.console.append_text("Ready to play.\nWaiting for your opponent...\n")
+
+    def play_game(self, gamedata):
+        self.console.append_text("\nStarting game: %s\n\n\n" %gamedata.game_type)
+        time.sleep(1.0)
+        self.player.game_topic_lock.acquire()
+        if self.player.game_topic is '':
+            is_first_player = (gamedata.first_player == self.player.player_id)
+            is_second_player = (gamedata.second_player == self.player.player_id)
+            if is_first_player or is_second_player:
+                gts = GtkThreadSafe()
+                # Register the game playing topic
+                game_topic = gamedata.game_topic
+                self.player.is_first = is_first_player
+                self.player.game_topic = game_topic
+                game_type = str(gamedata.game_type)
+                if game_type == "TrustGame":
+                    #self.the_game = TrustGame(self)
+                    with gts:
+                        print 'Start TrustGame'
+                elif game_type == "Prisoners":
+                    #self.the_game = Prisoners(self)
+                    with gts:
+                        print 'Start Prisoners'
+                elif game_type == "Ultimatum":
+                    with gts:
+                        self.the_game = UltimatumGame(self.console, self.player)
+                        self.content_vbox.pack_start(self.the_game.frame, expand=False)
+
+                else:
+                    with gts:
+                        print "Didnt match game_type: %s" %game_type
+                self.the_game.take_first_turn()
+        else:
+            print "skipped a game message from master, because I'm already in a game"
+        self.player.game_topic_lock.release()
 
 if __name__ == "__main__":
-    SpinButtonExample()
+    g = GameFacesUI()
     gtk.main()
