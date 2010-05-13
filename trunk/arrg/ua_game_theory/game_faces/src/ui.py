113b8c0695b77b999a562a19dcbed0804a5c570f
Cody.D.Jorgensen <Cody.D.Jorgensen@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-05-13 23:06:27 +0000

    Ultimatum is playable

--- trunk/arrg/ua_game_theory/game_faces/src/ui.py
+++ trunk/arrg/ua_game_theory/game_faces/src/ui.py
@@ -60,7 +60,8 @@ class GFPlayer:
         self.game_topic_lock = threading.Lock()
 
 class UltimatumGame:
-    def __init__(self, shared_console, player):
+    def __init__(self, parent, shared_console, player):
+        self.parent = parent
         self.shared_console = shared_console
         self.player = player
         
@@ -100,7 +101,13 @@ class UltimatumGame:
         self.toggle_user_interaction()
     
     def ok_button_clicked(self, widget, data=None):
-        self.shared_console.append_text('hello world\n')
+        offer = self.spin_bid.get_value_as_int()
+        self.set_balance(10 - offer)
+        gp = GamePlay(play_number=1,amount=offer, player_id=self.player.player_id)
+        gp.header.stamp = rospy.Time.now()
+        self.play_pub.publish(gp)
+        self.toggle_user_interaction()
+        self.shared_console.append_text('You sent %d.\nWaiting for second player...\n' %offer)
     
     def set_balance(self, balance):
         self.label_balance_amt.set_markup('<span size="20000" weight="bold">%d</span>' %balance)
@@ -124,45 +131,54 @@ class UltimatumGame:
                     self.set_balance(10)
                     self.toggle_user_interaction()
                     self.shared_console.append_text("Choose an amount, between 0 and 10 and press OK\n")
-#                gp = GamePlay(play_number=1,amount=offer, player_id=self.player_id)
-#                gp.header.stamp = rospy.Time.now()
-#                self.play_pub.publish(gp)
-#                print "Waiting for second player"
             else:
                 with gts:
                     self.shared_console.append_text("Waiting for first player...\n")
-#        elif play_number == 1:
-#            if self.player.is_first_player:
-#                pass    
-#            else:
-#                print "Player 1 sent "+str(game_play.amount)
-#                print "You can either accept or reject this offer"
-#                choice = raw_input("Please, input 'A' to accept or 'R' to reject the offer")
-#                if choice == "A":
-#                    self.current_amount = str(game_play.amount)
-#                else:
-#				    self.current_amount = 0
-#                print "Your payoff is now: " + str(self.current_amount)
-#                gp = GamePlay(play_number=2,amount=ord(choice), player_id=self.player_id)
-#                gp.header.stamp = rospy.Time.now()
-#                self.play_pub.publish(gp)
-#        elif play_number == 2:
-#            if self.player.is_first_player:
-#                choice = chr(game_play.amount)
-#                if choice == 'A':
-#                    which_choice = "Accepted"
-#                    self.current_amount = self.original - self.offer
-#                else:
-#                    which_choice = "Rejected"
-#                    self.current_amount = 0
-#                print "The other player decide to" + str(which_choice) + "your offer"
-#                print "Your payoff for this round is:" + str(self.current_amount)
-#                gp = GamePlay(play_number=3,amount=-1, player_id=self.player_id)
-#                gp.header.stamp = rospy.Time.now(); self.play_pub.publish(gp)  
-#            else:
-#                pass
-#            print "Please wait for the next game to start"
-#            self.unregister_game()
+        elif play_number == 1:
+            if self.player.is_first:
+                with gts:
+                    print 'First Player // Turn 2 // Waiting'   
+            else:
+                with gts:
+                    label_prompt = gtk.Label('Player 1 has sent %d.\nYou can either accept or reject this offer.' %game_play.amount)
+                    dialog = gtk.Dialog('Choose One', self.parent, gtk.DIALOG_MODAL,
+                                    ('Reject', gtk.RESPONSE_REJECT,
+                                      'Accept', gtk.RESPONSE_ACCEPT)) 
+                    dialog.vbox.pack_start(label_prompt, True, True, 0)
+                    label_prompt.show()
+                    dialog.connect("delete_event", lambda w, e : None)
+                    response = gtk.RESPONSE_DELETE_EVENT
+                    while response == gtk.RESPONSE_DELETE_EVENT:
+                        response = dialog.run()
+                    dialog.destroy()
+                    if response == gtk.RESPONSE_REJECT:
+                        new_balance = 0
+                    elif response == gtk.RESPONSE_ACCEPT:
+                        new_balance = game_play.amount
+                    self.set_balance(new_balance)
+                    self.shared_console.append_text('Your payoff is now: %d' %new_balance)
+                    gp = GamePlay(play_number=2,amount=new_balance, player_id=self.player.player_id)
+                    gp.header.stamp = rospy.Time.now()
+                    self.play_pub.publish(gp)
+        elif play_number == 2:
+            if self.player.is_first:
+                with gts:
+                    self.shared_console.append_text('Your offer was ')
+                    if game_play.amount == 0:
+                        self.shared_console.append_text('rejected. Your payoff is now 0.\n')
+                        self.set_balance(0)
+                    else:
+                        self.shared_console.append_text('accepted. Your payoff is now %d.\n' %game_play.amount)
+                        self.set_balance(10 - game_play.amount)
+                    gp = GamePlay(play_number=3,amount=-1, player_id=self.player.player_id)
+                    gp.header.stamp = rospy.Time.now()
+                    self.play_pub.publish(gp)
+            else:
+                with gts:
+                    print 'Player 2 // Turn 3 // Game Over'
+            with gts:
+                self.shared_console.append_text("\n\nPlease wait for the next game to start.\n")
+            self.unregister_game()
     
     def unregister_game(self):
         self.player.game_topic_lock.acquire()
@@ -203,20 +219,21 @@ class GameFacesUI:
     def __init__(self):
         # ivars
         self.player = GFPlayer(-1, False, '')
+        self.the_game = None
     
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.connect("destroy", lambda w: gtk.main_quit())
-        window.set_title("Game")
-        window.resize(500, 250)
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("destroy", lambda w: gtk.main_quit())
+        self.window.set_title("Game")
+        self.window.resize(500, 250)
 
         self.content_vbox = gtk.VBox(False, 8)
         self.content_vbox.set_border_width(10)
-        window.add(self.content_vbox)
+        self.window.add(self.content_vbox)
         
         self.console = GFConsole()
         self.content_vbox.pack_end(self.console.frame)
         
-        window.show_all()
+        self.window.show_all()
         self.register_player()
         
     def register_player(self):
@@ -236,6 +253,8 @@ class GameFacesUI:
         self.console.append_text("Ready to play.\nWaiting for your opponent...\n")
 
     def play_game(self, gamedata):
+        if self.the_game:
+            self.content_vbox.remove(self.the_game.frame)
         self.console.append_text("\nStarting game: %s\n\n\n" %gamedata.game_type)
         time.sleep(1.0)
         self.player.game_topic_lock.acquire()
@@ -250,16 +269,16 @@ class GameFacesUI:
                 self.player.game_topic = game_topic
                 game_type = str(gamedata.game_type)
                 if game_type == "TrustGame":
-                    #self.the_game = TrustGame(self)
+                    self.the_game = None #TrustGame(self)
                     with gts:
                         print 'Start TrustGame'
                 elif game_type == "Prisoners":
-                    #self.the_game = Prisoners(self)
+                    self.the_game = None #Prisoners(self)
                     with gts:
                         print 'Start Prisoners'
                 elif game_type == "Ultimatum":
                     with gts:
-                        self.the_game = UltimatumGame(self.console, self.player)
+                        self.the_game = UltimatumGame(self.window, self.console, self.player)
                         self.content_vbox.pack_start(self.the_game.frame, expand=False)
 
                 else:
@@ -267,7 +286,8 @@ class GameFacesUI:
                         print "Didnt match game_type: %s" %game_type
                 self.the_game.take_first_turn()
         else:
-            print "skipped a game message from master, because I'm already in a game"
+            with gts:
+                print "skipped a game message from master, because I'm already in a game"
         self.player.game_topic_lock.release()
 
 if __name__ == "__main__":
