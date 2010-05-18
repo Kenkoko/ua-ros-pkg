6c705e7d53e36f0fae4216dde2e3884e27e1266d
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-05-18 07:22:49 +0000

    Clean up bg averager.

--- arrg/ua_vision/background_filters/src/background_sub.cpp
+++ arrg/ua_vision/background_filters/src/background_sub.cpp
@@ -1,5 +1,39 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Antons Rebguns.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
 #include <cv_bridge/CvBridge.h>
 #include <boost/thread/thread.hpp>
 #include <math.h>
@@ -8,6 +42,8 @@
 #include <cv.h>
 #include <cxcore.h>
 #include <highgui.h>
+
+#include "background_filters/common.h"
 
 #define TWO_PI 6.28318531
 
@@ -23,54 +59,7 @@ vector<float> std_dev;
 ros::Subscriber image_sub;
 ros::Publisher prob_img_pub;
 
-string colorspace = "rgchroma";
-
-void print_mat(CvMat *A)
-{
-    int i, j;
-    for (i = 0; i < A->rows; i++)
-    {
-        printf("\n");
-        switch (CV_MAT_DEPTH(A->type))
-        {
-            case CV_32F:
-            case CV_64F:
-                for (j = 0; j < A->cols; j++)
-                printf ("%8.6f ", (float)cvGetReal2D(A, i, j));
-                break;
-            case CV_8U:
-            case CV_16U:
-                for(j = 0; j < A->cols; j++)
-                printf ("%6d",(int)cvGetReal2D(A, i, j));
-                break;
-            default:
-            break;
-        }
-    }
-    printf("\n");
-}
-
-void convertToChroma(IplImage *in_bgr, IplImage *out_rgchroma)
-{
-    int img_height = in_bgr->height;
-    int img_width = in_bgr->width;
-    
-    for (int y = 0; y < img_height; ++y)
-    {
-        uchar* ptr = (uchar *) (in_bgr->imageData + y * in_bgr->widthStep);
-        float* out_ptr = (float *) (out_rgchroma->imageData + y * out_rgchroma->widthStep);
-
-        for (int x = 0; x < img_width; ++x)
-        {
-            float b = ptr[3*x+0];
-            float g = ptr[3*x+1];
-            float r = ptr[3*x+2];
-            
-            out_ptr[2*x+0] = r / (1 + b + g + r);
-            out_ptr[2*x+1] = g / (1 + b + g + r);
-        }
-    }
-}
+string colorspace;
 
 bool first = true;
 
@@ -78,7 +67,6 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
 {
     sensor_msgs::CvBridge bridge;
     IplImage *bg = NULL;
-    cout << colorspace << endl;
 
     try
     {
@@ -106,17 +94,17 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
     {
         ROS_ERROR("CvBridgeError");
     }
-    
+
     IplImage *prob_img = cvCreateImage(cvGetSize(bg), IPL_DEPTH_32F, 1);
     float *prob_data = (float *) prob_img->imageData;
 
     int width = bg->width;
     int height = bg->height;
-    
+
     CvMat *bgr_new = NULL;
     CvMat *bgr_ave = NULL;
     CvMat *inv_cov = NULL;
-    
+
     if (colorspace == "rgb" || colorspace == "hsv")
     {
         bgr_new = cvCreateMat(1, 3, CV_32FC1);
@@ -129,31 +117,31 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
         bgr_ave = cvCreateMat(1, 2, CV_32FC1);
         inv_cov = cvCreateMatHeader(2, 2, CV_32FC1);
     }
-    
+
     for (int y = 0; y < height; ++y)
     {
         if (colorspace == "rgb" || colorspace == "hsv")
         {
             uchar* ptr_bg = (uchar *) (bg->imageData + y * bg->widthStep);
             uchar* ptr_ave = (uchar *) (ave_bg->imageData + y * ave_bg->widthStep);
-        
+
             for (int x = 0; x < width; ++x)
             {
                 cvSet1D(bgr_new, 0, cvScalar(ptr_bg[3*x+0]));
                 cvSet1D(bgr_new, 1, cvScalar(ptr_bg[3*x+1]));
                 cvSet1D(bgr_new, 2, cvScalar(ptr_bg[3*x+2]));
-                
+
                 cvSet1D(bgr_ave, 0, cvScalar(ptr_ave[3*x+0]));
                 cvSet1D(bgr_ave, 1, cvScalar(ptr_ave[3*x+1]));
                 cvSet1D(bgr_ave, 2, cvScalar(ptr_ave[3*x+2]));
-                
+
                 cvInitMatHeader(inv_cov, 3, 3, CV_32FC1, &cov_mats_inv[(y*width+x)*9]);
-                
+
                 double mah_dist = cvMahalanobis(bgr_new, bgr_ave, inv_cov);
                 double unnorm_gaussian = exp(-0.5 * mah_dist);
                 double partition = 1.0 / (pow(TWO_PI, 1.5) * sqrt(dets[y*width+x]));
                 float p = partition * unnorm_gaussian;
-                
+
                 prob_data[y*width+x] = p;
             }
         }
@@ -166,17 +154,17 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
             {
                 cvSet1D(bgr_new, 0, cvScalar(ptr_bg[2*x+0]));
                 cvSet1D(bgr_new, 1, cvScalar(ptr_bg[2*x+1]));
-                
+
                 cvSet1D(bgr_ave, 0, cvScalar(ptr_ave[2*x+0]));
                 cvSet1D(bgr_ave, 1, cvScalar(ptr_ave[2*x+1]));
-                
+
                 cvInitMatHeader(inv_cov, 2, 2, CV_32FC1, &cov_mats_inv[(y*width+x)*4]);
-                
+
                 double mah_dist = cvMahalanobis(bgr_new, bgr_ave, inv_cov);
                 double unnorm_gaussian = exp(-0.5 * mah_dist);
                 double partition = 1.0 / (TWO_PI * sqrt(dets[y*width+x]));
                 float p = partition * unnorm_gaussian;
-                
+
                 //if (y*width+x < 100) cout << mah_dist << ", " << p << endl;
                 prob_data[y*width+x] = p;
 
@@ -186,19 +174,19 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
                     cout << ptr_ave[2*x+0] << endl;
                     cout << ptr_ave[2*x+1] << endl;
                 }
-                
+
             }
         }
     }
-    
+
     cvReleaseMat(&bgr_new);
     cvReleaseMat(&bgr_ave);
     cvReleaseMat(&inv_cov);
-    
+
     if (colorspace == "rgb" || colorspace == "hsv")
     {
         cvLog(prob_img, prob_img);
-        
+
         double min, max;
         cvMinMaxLoc(prob_img, &min, &max);
         cvConvertScale(prob_img, prob_img, 1.0 / (max - min), -min / (max - min));
@@ -211,16 +199,9 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg_ptr)
     }
 
     cvShowImage("prob_img", prob_img);
-    cvWaitKey(100);
-//    if (first)
-//    {
-//        CvMat* m = cvCreateMatHeader(prob_img->height, prob_img->width, CV_32FC1);
-//        cvGetMat(prob_img, m);
-//        print_mat(m);
-//        first = false;
-//    }
+
     prob_img_pub.publish(sensor_msgs::CvBridge::cvToImgMsg(prob_img));
-    
+
     cvReleaseImage(&bg);
     prob_data = NULL;
     cvReleaseImage(&prob_img);
@@ -230,9 +211,9 @@ int main (int argc, char **argv)
 {
     ros::init(argc, argv, "background_sub");
     ros::NodeHandle n;
-    
+
     ros::ServiceClient client = n.serviceClient<background_filters::GetBgStats>("get_background_stats");
-    
+
     background_filters::GetBgStats srv;
     sensor_msgs::CvBridge b;
 
@@ -244,19 +225,10 @@ int main (int argc, char **argv)
 
         b.fromImage(srv.response.average_background);
         ave_bg = b.toIpl();
-        
-        for (int y = 0; y < ave_bg->height; ++y)
-        {
-            float* ptr = (float*) (ave_bg->imageData + y * ave_bg->widthStep);
 
-            for (int x = 0; x < ave_bg->width; ++x)
-            {
-                int pixel = y * ave_bg->width + x;
-                cout << "[" << pixel << "] = " << "(" << ptr[2*x+0] << ", " << ptr[2*x+1] << ")" << endl;
-            }
-        }
+        print_img(ave_bg);
 
-        
+        colorspace = srv.response.colorspace;
         cov_mats = srv.response.covariance_matrix;
         cov_mats_inv = srv.response.covariance_matrix_inv;
         dets = srv.response.covariance_matrix_dets;
@@ -269,7 +241,7 @@ int main (int argc, char **argv)
         ROS_ERROR("Failed to call service get_bg_stats");
         return 1;
     }
-    
+
     cout << "subscribing" << endl;
     image_sub = n.subscribe("image", 1, handle_image);
     ros::Publisher ave_bg_pub = n.advertise<sensor_msgs::Image>("average_bg_service", 1);
@@ -284,6 +256,7 @@ int main (int argc, char **argv)
 //        r.sleep();
 //    }
 
+    cvStartWindowThread();
     ros::spin();
 
     return 0;
