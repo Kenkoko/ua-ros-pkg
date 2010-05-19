c747906976a7e530c0c3eda44edc17a459e50775
Cody.D.Jorgensen <Cody.D.Jorgensen@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-05-19 20:16:52 +0000

    prisoners is playable.

--- trunk/arrg/ua_game_theory/game_faces/src/ui.py
+++ trunk/arrg/ua_game_theory/game_faces/src/ui.py
@@ -281,6 +281,199 @@ class TrustGameController:
         self.player.game_topic = ""
         self.player.game_topic_lock.release()
 
+class PrisonersGameController:
+    def __init__(self, parent, shared_console, player):
+        self.parent = parent
+        self.shared_console = shared_console
+        self.player = player
+        self.balance = 0
+        self.payoffs = (([10,10],[12,2]),([2,12],[2,2]))
+        self.row_choice = 2 # choose a number out of range so we know if we dont get a value
+        self.col_choice = 2 # choose a number out of range so we know if we dont get a value
+        
+        self.enabled = True
+        self.play_pub = rospy.Publisher(self.player.game_topic, GamePlay, subscriber_listener=None, tcp_nodelay=True, latch=(not self.player.is_first))
+        self.play_sub = rospy.Subscriber(self.player.game_topic, GamePlay, self.take_turn)
+        
+        self.title = "Prisoners"
+        self.view = gtk.Frame(self.title)
+        game_vbox = gtk.VBox(False, 8)
+        self.view.add(game_vbox)
+        
+        self.box_status = gtk.HBox(False, 8)
+        game_vbox.pack_start(self.box_status, expand=False, fill=False)
+        
+        self.label_balance_desc = gtk.Label('Your current balance is')
+        self.box_status.add(self.label_balance_desc)
+        
+        self.label_balance_amt = gtk.Label()
+        self.set_balance(0)
+        self.box_status.add(self.label_balance_amt)
+
+        self.box_control = gtk.HBox(False, 8)
+        game_vbox.pack_start(self.box_control, expand=False, fill=False)
+        
+        self.button_top = None
+        self.button_bottom = None
+        self.button_left = None
+        self.button_right = None
+
+        table_cells = []
+
+        if self.player.is_first:
+            nrows = 2
+            ncols = 3
+            
+            self.button_top = gtk.CheckButton("Top")
+            self.button_top.connect("toggled", self.checkbox_toggled, "Top")
+            table_cells.append(self.button_top)
+            
+            self.button_bottom = gtk.CheckButton("Bottom")
+            self.button_bottom.connect("toggled", self.checkbox_toggled, "Bottom")
+            table_cells.append(self.button_bottom)
+        else:
+            nrows = 3
+            ncols = 2
+            
+            self.button_left = gtk.CheckButton("Left")
+            self.button_left.connect("toggled", self.checkbox_toggled, "Left")
+            table_cells.append(self.button_left)
+            
+            self.button_right = gtk.CheckButton("Right")
+            self.button_right.connect("toggled", self.checkbox_toggled, "Right")
+            table_cells.append(self.button_right)
+        
+        self.table = gtk.Table(rows=nrows, columns=ncols, homogeneous=False)
+        
+        self.label_tl = gtk.Label("10 | 10")
+        table_cells.append(self.label_tl)
+        
+        self.label_tr = gtk.Label("12 | 2")
+        table_cells.append(self.label_tr)
+        
+        self.label_bl = gtk.Label("2 | 12")
+        table_cells.append(self.label_bl)
+        
+        self.label_br = gtk.Label("2 | 2")
+        table_cells.append(self.label_br)
+        
+        table_cells.reverse()
+        
+        if self.player.is_first:
+            i_max = ncols
+            j_max = nrows
+        else:
+            i_max = nrows
+            j_max = ncols
+        for i in xrange(i_max):
+            for j in xrange(j_max):
+                if self.player.is_first:
+                    self.table.attach(table_cells.pop(), i, i + 1, j, j + 1, xoptions=gtk.EXPAND)
+                else:
+                    self.table.attach(table_cells.pop(), j, j + 1, i, i + 1, xoptions=gtk.EXPAND)
+        
+        self.box_control.add(self.table)
+        
+        self.button_bid = gtk.Button(stock=gtk.STOCK_OK)
+        self.button_bid.set_size_request(80, 30)
+        self.button_bid.connect("clicked", self.ok_button_clicked)
+        self.box_control.add(self.button_bid)
+        
+        self.view.show_all()
+        self.toggle_user_interaction()
+    
+    def checkbox_toggled(self, widget, data=None):
+        if widget == self.button_left:
+            self.col_choice = 0 if widget.get_active() else 2
+            self.button_right.set_sensitive(not widget.get_active())
+        elif widget == self.button_right:
+            self.col_choice = 1 if widget.get_active() else 2
+            self.button_left.set_sensitive(not widget.get_active())
+        elif widget == self.button_top:
+            self.row_choice = 0 if widget.get_active() else 2
+            self.button_bottom.set_sensitive(not widget.get_active())
+        elif widget == self.button_bottom:
+            self.row_choice = 1 if widget.get_active() else 2
+            self.button_top.set_sensitive(not widget.get_active())
+    
+    def ok_button_clicked(self, widget, data=None):
+        self.toggle_user_interaction()
+        if self.player.is_first:
+            play_num = 1
+            amt_val = self.row_choice
+            self.shared_console.append_text('Waiting for second player...\n')
+        else:
+            play_num = 2
+            amt_val = self.col_choice
+            r_str = ('top', 'bottom')[self.row_choice]
+            c_str = ('left', 'right')[self.col_choice]
+            self.shared_console.append_text('You chose the %s column and player 1 chose the %s row.\n' %(c_str, r_str))
+            new_balance = self.payoffs[self.row_choice][self.col_choice][self.player.is_first]
+            self.set_balance(new_balance)
+        gp = GamePlay(play_number=play_num,amount=amt_val, player_id=self.player.player_id)
+        gp.header.stamp = rospy.Time.now()
+        self.play_pub.publish(gp)
+    
+    def set_balance(self, balance):
+        self.label_balance_amt.set_markup('<span size="20000" weight="bold">%d</span>' %balance)
+    
+    def toggle_user_interaction(self):
+        self.enabled = not self.enabled
+        self.box_control.set_sensitive(self.enabled)
+    
+    def take_first_turn(self):
+        if not self.player.is_first:
+            gp = GamePlay(play_number=0,amount=0, player_id=self.player.player_id)
+            gp.header.stamp = rospy.Time.now()
+            self.play_pub.publish(gp)
+        
+    def take_turn(self, game_play):
+        gts = GtkThreadSafe()
+        play_number = game_play.play_number
+        if play_number == 0:
+            if self.player.is_first:
+                with gts:
+                    self.toggle_user_interaction()
+                    self.shared_console.append_text("Your possible payoffs are on the left in each cell.\n")
+                    self.shared_console.append_text("Please choose the top row or the bottom row and press OK.\n")
+            else:
+                with gts:
+                    self.shared_console.append_text("Waiting for first player...\n")
+        elif play_number == 1:
+            if self.player.is_first:
+                pass
+            else:
+                with gts:
+                    self.row_choice = game_play.amount
+                    self.shared_console.append_text("Your possible payoffs are on the right in each cell.\n")
+                    self.shared_console.append_text("Please choose the left column or the right column and press OK.\n")
+                    self.toggle_user_interaction()
+        elif play_number == 2:
+            if self.player.is_first:
+                with gts:
+                    self.col_choice = game_play.amount
+                    r_str = ('top', 'bottom')[self.row_choice]
+                    c_str = ('left', 'right')[self.col_choice]
+                    self.shared_console.append_text('You chose the %s row and player 2 chose the %s column.\n' %(r_str, c_str))
+                    new_balance = self.payoffs[self.row_choice][self.col_choice][self.player.is_first]
+                    self.shared_console.append_text('Your payoff is now %d.\n' %new_balance)
+                    self.set_balance(new_balance)
+                    gp = GamePlay(play_number=3,amount=-1, player_id=self.player.player_id)
+                    gp.header.stamp = rospy.Time.now()
+                    self.play_pub.publish(gp)
+            else:
+                pass
+            with gts:
+                self.shared_console.append_text("\n\nPlease wait for the next game to start.\n")
+            self.unregister_game()
+    
+    def unregister_game(self):
+        self.player.game_topic_lock.acquire()
+        self.play_pub.unregister()
+        self.play_sub.unregister()
+        self.player.game_topic = ""
+        self.player.game_topic_lock.release()
+
 class GFConsoleController:
     def __init__(self):
         self.view = gtk.Frame()
@@ -368,9 +561,8 @@ class GameFacesUI:
                         self.content_vbox.pack_start(self.the_game_controller.view, expand=False)
                 elif game_type == "Prisoners":
                     with gts:
-#                        self.the_game_controller = PrisonersGameController(self.window, self.console, self.player)
-#                        self.content_vbox.pack_start(self.the_game_controller.view, expand=False)
-                        print 'Start a Prisoners Game!'
+                        self.the_game_controller = PrisonersGameController(self.window, self.console, self.player)
+                        self.content_vbox.pack_start(self.the_game_controller.view, expand=False)
                 elif game_type == "Ultimatum":
                     with gts:
                         self.the_game_controller = UltimatumGameController(self.window, self.console, self.player)
