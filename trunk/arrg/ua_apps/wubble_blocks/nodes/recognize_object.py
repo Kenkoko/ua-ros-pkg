a0992d55d647e8fde2947b321c0136f1ed8d5770
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-08-03 00:49:22 +0000

    Fix closing in on the object from laser data.

--- trunk/arrg/ua_apps/wubble_blocks/nodes/recognize_object.py
+++ trunk/arrg/ua_apps/wubble_blocks/nodes/recognize_object.py
@@ -33,6 +33,9 @@
 import roslib; roslib.load_manifest('wubble_blocks')
 import rospy
 
+from math import sqrt
+from math import pow
+
 from wubble_blocks.srv import ClassifyObject
 from object_tracking.srv import GetTracksInterval
 
@@ -41,20 +44,36 @@ class ObjectClassifier:
         self.classify_obj_server = rospy.Service('classify_object', ClassifyObject, self.classify)
         self.get_tracks_srv = rospy.ServiceProxy('get_tracks_interval', GetTracksInterval)
         rospy.wait_for_service('get_tracks_interval')
-    
+        self.balls = 26
+        self.blocks = 12
+
     def classify(self, req):
-        resp = self.get_tracks_srv(req.id, req.swat_time, req.swat_time + rospy.Duration(5.0))
-        return self.categorize_track(resp.tracks[0])
-    
+        resp = self.get_tracks_srv(req.id, req.swat_time, req.swat_time + rospy.Duration(0.5))
+        return self.categorize_track(resp.track)
+
     def categorize_track(self, track):
-        # TODO: put actual classification algorithm here
-        return "Ball"
+        start_x = track.waypoints[0].pixel.x
+        start_y = track.waypoints[0].pixel.y
+        end_x = track.waypoints[-1].pixel.x
+        end_y = track.waypoints[-1].pixel.y
+        dist = sqrt(pow(end_x - start_x, 2.0) + pow(end_y - start_y, 2.0))
+
+        rospy.loginfo("Object [%d] has %d waypoints in its track (%d.%d to %d.%d)",
+                      track.id, len(track.waypoints),
+                      track.waypoints[0].header.stamp.secs, track.waypoints[0].header.stamp.nsecs,
+                      track.waypoints[-1].header.stamp.secs, track.waypoints[-1].header.stamp.nsecs)
+        rospy.loginfo('Distance traveled in 0.5 seconds is %f', dist)
+
+        if abs(self.balls - dist) < abs(self.blocks - dist):
+            return "Ball"
+        else:
+            return "Block"
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('object_classifier', anonymous=True)        
+        rospy.init_node('object_classifier', anonymous=True)
         swat = ObjectClassifier()
         rospy.spin()
-        
+
     except rospy.ROSInterruptException:
         pass
