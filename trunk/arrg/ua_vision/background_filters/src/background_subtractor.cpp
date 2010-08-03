b6c4302c7e1dcd019930db68fac2484f8c83ff3d
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-08-03 23:00:08 +0000

    Forgot include for printf.

--- trunk/arrg/ua_vision/background_filters/src/background_subtractor.cpp
+++ trunk/arrg/ua_vision/background_filters/src/background_subtractor.cpp
@@ -32,6 +32,8 @@
 *  POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************/
 
+#include <cstdio>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
