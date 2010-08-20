a3c1023d4b59894414a61b771c5a5a08f158beb0
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-08-20 06:33:47 +0000

    Handle morepossible responses

--- arrg/ua_experimental/chr_6dm_imu/src/chr_6dm_imu/chr_6dm_io.py
+++ arrg/ua_experimental/chr_6dm_imu/src/chr_6dm_imu/chr_6dm_io.py
@@ -50,123 +50,101 @@ class CHR6dmIMU(object):
 
     def __init__(self, port='/dev/ttyUSB0'):
         """ Constructor takes serial port as argument. """
-        try:
-            self.ser = None
-            self.ser = serial.Serial(port)
-            self.ser.timeout = 0.015
-            self.ser.baudrate = 115200
-            self.ser.bytesize = serial.EIGHTBITS
-            self.ser.stopbits = serial.STOPBITS_ONE
-            self.ser.parity = serial.PARITY_NONE
-            print "Connected to device"
-        except serial.SerialException as e:
-            print e
-           
-        self.rx_buf_idx = 0
-        self.RX_BUF_SIZE = 500
-        self.MAX_PACKET_SIZE = 40
-        self.CHANNEL_COUNT = 15
+        self.ser = None
+        self.ser = serial.Serial(port)
+        self.ser.timeout = 0.015
+        self.ser.baudrate = 115200
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.stopbits = serial.STOPBITS_ONE
+        self.ser.parity = serial.PARITY_NONE
+        
+        print "Connected to IMU on %s" % port
 
     def __del__(self):
         """ Destructor calls self.close_serial_port() """
         self.close()
 
-    def write_to_imu(self, command, data=tuple()):
-        self.ser.flushInput()
-
+    def calculate_checksum(self, command, data):
         chkSum = reduce(int.__add__, map(ord, ['s', 'n', 'p']), 0)
         chkSum = chkSum + command + len(data) + reduce(int.__add__, data, 0)
-        dataStr = ''.join(map(chr, data))
+        return chkSum
 
+    def write_to_imu(self, command, data=tuple()):
+        self.ser.flushInput()
+        
+        chkSum = self.calculate_checksum(command, data)
+        dataStr = ''.join(map(chr, data))
+        
         # packet: s  n  p  INSTRUCTION  LENGTH  DATA  CHECKSUM
         # bytes:  1  1  1       1          1     N       2
-        packetStr = ('s' + 'n' + 'p' +
-                    chr(command) +
-                    chr(len(dataStr)) +
-                    dataStr +
-                    chr(chkSum >> 8) + chr(chkSum & 0x0FF))
-        p = map(ord, packetStr)
-        print 'Wrote packet: ' + str(p)
-        
+        packetStr = 'snp' + chr(command) + chr(len(dataStr)) + dataStr + chr(chkSum >> 8) + chr(chkSum & 0x0FF)
         self.ser.write(packetStr)
-
+        
+        print 'Command: %s, data packet: %s' % (hex(command).upper(), str(data))
+        
         # wait for response
-        #time.sleep(0.01)
+        time.sleep(0.005)
+        
+        self.read_from_imu()
 
     def read_from_imu(self):
         # look for packet prefix 'snp'
         while self.ser.read() != 's':
             print "looking for packet prefix 'snp'"
         packet = 's' + self.ser.read(2) # read 'n' and 'p'
-
+        
         if packet != 'snp':
             print "Received corrupted packet, prefix was %s" % packet
             return
-
+            
         command = ord(self.ser.read())
-        print "command = %s" % hex(command).upper()
-
         n = ord(self.ser.read())
-        print "data bytes = %d" % n
-
+        
         dataStr = self.ser.read(n)
         data = map(b2a_hex, dataStr)
         data = map(int, data, [16] * len(data))
-        print data
-
+        
+        print "Received reply: %s, data: %s" % (hex(command).upper(), str(data))
+        
         chkSumStr = self.ser.read(2)
         chkSumData = map(b2a_hex, chkSumStr)
         chkSumData = map(int, chkSumData, [16] * len(chkSumData))
         chkSumRx = (chkSumData[0] << 8) | chkSumData[1]
-
-        chkSum = reduce(int.__add__, map(ord, ['s', 'n', 'p']), 0)
-        chkSum = chkSum + command + len(data) + reduce(int.__add__, data, 0)
+        
+        chkSum = self.calculate_checksum(command, data)
         
         if chkSumRx != chkSum:
             print "Checksums don't match %d != %d" % (chkSumRx, chkSum)
             return
-
+            
         if command == COMMAND_COMPLETE:
-            print 'command complete'
+            print 'Command %s complete' % hex(data[0]).upper()
+        elif command == COMMAND_FAILED:
+            print 'Command %s failed' % hex(data[0]).upper()
         elif command == BAD_CHECKSUM:
-            print 'bad checksum'
+            print 'Bad checksum'
         elif command == BAD_DATA_LENGTH:
-            print 'bad data length'
+            print 'Bad data length for command %s' % hex(data[0]).upper()
         elif command == UNRECOGNIZED_PACKET:
-            print 'unrecognized command'
+            print 'Unrecognized packet %s' % hex(data[0]).upper()
+        elif command == BUFFER_OVERFLOW:
+            print 'Buffer overflow'
+        elif command == STATUS_REPORT:
+            print 'Self test status report'
+            if (data[0] >> 5) & 0x01:
+                print 'FAILED self-test: gyro_z'
+            if (data[0] >> 4) & 0x01:
+                print 'FAILED self-test: gyro_y'
+            if (data[0] >> 3) & 0x01:
+                print 'FAILED self-test: gyro_x'
+            if (data[0] >> 2) & 0x01:
+                print 'FAILED self-test: accel_z'
+            if (data[0] >> 1) & 0x01:
+                print 'FAILED self-test: accel_y'
+            if data[0] & 0x01:
+                print 'FAILED self-test: accel_x'
         elif command == SENSOR_DATA:
             active_channels = (data[0] << 8) | data[1]
-            
-            if (active_channels >> 15) & 0x01:
-                print 'yaw channel active'
-            if (active_channels >> 14) & 0x01:
-                print 'pitch channel active'
-            if (active_channels >> 13) & 0x01:
-                print 'roll channel active'
-            if (active_channels >> 12) & 0x01:
-                print 'yaw rate channel active'
-            if (active_channels >> 11) & 0x01:
-                print 'pitch rate channel active'
-            if (active_channels >> 10) & 0x01:
-                print 'roll rate channel active'
-            if (active_channels >> 9) & 0x01:
-                print 'mx channel active'
-            if (active_channels >> 8) & 0x01:
-                print 'my channel active'
-            if (active_channels >> 7) & 0x01:
-                print 'mz channel active'
-            if (active_channels >> 6) & 0x01:
-                print 'gx channel active'
-            if (active_channels >> 5) & 0x01:
-                print 'gy channel active'
-            if (active_channels >> 4) & 0x01:
-                print 'gz rate channel active'
-            if (active_channels >> 3) & 0x01:
-                print 'ax rate channel active'
-            if (active_channels >> 2) & 0x01:
-                print 'ay rate channel active'
-            if (active_channels >> 1) & 0x01:
-                print 'az channel active'
             
             i = 2
             
@@ -274,13 +252,145 @@ class CHR6dmIMU(object):
                 az = value[0] * SCALE_ACCEL_Z * MILIG_TO_MSS
                 print 'az = %f' % az
                 i += 2
+        elif command == GYRO_BIAS_REPORT:
+            value = struct.unpack('>h', dataStr[0] + dataStr[1])
+            gyro_z_bias = value[0] * SCALE_GYRO_Z * DEG_TO_RAD
+            
+            value = struct.unpack('>h', dataStr[2] + dataStr[3])
+            gyro_y_bias = value[0] * SCALE_GYRO_Y * DEG_TO_RAD
+            
+            value = struct.unpack('>h', dataStr[4] + dataStr[5])
+            gyro_x_bias = value[0] * SCALE_GYRO_X * DEG_TO_RAD
+            
+            print 'gyro biases: (%f, %f, %f)' % (gyro_x_bias, gyro_y_bias, gyro_z_bias)
+        elif command == GYRO_SCALE_REPORT:
+            value = struct.unpack('>f', dataStr[0:4])
+            gyro_z_scale = value[0]
+            
+            value = struct.unpack('>f', dataStr[4:8])
+            gyro_y_scale = value[0]
+            
+            value = struct.unpack('>f', dataStr[8:12])
+            gyro_x_scale = value[0]
+            
+            print 'gyro scales: (%f, %f, %f)' % (gyro_x_scale, gyro_y_scale, gyro_z_scale)
+        elif command == START_CAL_REPORT:
+            if data[0] & 0x01:
+                print 'Gyro bias stratup calibration is ENABLED'
+            else:
+                print 'Gyro bias stratup calibration is DISABLED'
+        elif command == ACCEL_BIAS_REPORT:
+            value = struct.unpack('>h', dataStr[0] + dataStr[1])
+            accel_z_bias = value[0] * SCALE_ACCEL_Z * MILIG_TO_MSS
+            
+            value = struct.unpack('>h', dataStr[2] + dataStr[3])
+            accel_y_bias = value[0] * SCALE_ACCEL_Y * MILIG_TO_MSS
+            
+            value = struct.unpack('>h', dataStr[4] + dataStr[5])
+            accel_x_bias = value[0] * SCALE_ACCEL_X * MILIG_TO_MSS
+            
+            print 'accel biases: (%f, %f, %f)' % (accel_x_bias, accel_y_bias, accel_z_bias)
+        elif command == ACCEL_REF_VECTOR_REPORT:
+            value = struct.unpack('>h', dataStr[0] + dataStr[1])
+            accel_z_ref = value[0] * SCALE_ACCEL_Z * MILIG_TO_MSS
+            
+            value = struct.unpack('>h', dataStr[2] + dataStr[3])
+            accel_y_ref = value[0] * SCALE_ACCEL_Y * MILIG_TO_MSS
+            
+            value = struct.unpack('>h', dataStr[4] + dataStr[5])
+            accel_x_ref = value[0] * SCALE_ACCEL_X * MILIG_TO_MSS
+            
+            print 'accel ref vector: (%f, %f, %f)' % (accel_x_ref, accel_y_ref, accel_z_ref)
         elif command == ACTIVE_CHANNEL_REPORT:
-            print "active channel report received"
+            print "Active channel report received"
             active_channels = data[1] | (data[0] << 8)
-            print data
-            print active_channels
+            
+            if (active_channels >> 15) & 0x01:
+                print 'yaw channel active'
+            if (active_channels >> 14) & 0x01:
+                print 'pitch channel active'
+            if (active_channels >> 13) & 0x01:
+                print 'roll channel active'
+            if (active_channels >> 12) & 0x01:
+                print 'yaw rate channel active'
+            if (active_channels >> 11) & 0x01:
+                print 'pitch rate channel active'
+            if (active_channels >> 10) & 0x01:
+                print 'roll rate channel active'
+            if (active_channels >> 9) & 0x01:
+                print 'mx channel active'
+            if (active_channels >> 8) & 0x01:
+                print 'my channel active'
+            if (active_channels >> 7) & 0x01:
+                print 'mz channel active'
+            if (active_channels >> 6) & 0x01:
+                print 'gx channel active'
+            if (active_channels >> 5) & 0x01:
+                print 'gy channel active'
+            if (active_channels >> 4) & 0x01:
+                print 'gz rate channel active'
+            if (active_channels >> 3) & 0x01:
+                print 'ax rate channel active'
+            if (active_channels >> 2) & 0x01:
+                print 'ay rate channel active'
+            if (active_channels >> 1) & 0x01:
+                print 'az channel active'
+        elif command == ACCEL_COVARIANCE_REPORT:
+            value = struct.unpack('>f', dataStr[0:4])
+            accel_covariance = value[0]
+            print 'Accel covariance is %f' % accel_covariance
+        elif command == MAG_COVARIANCE_REPORT:
+            value = struct.unpack('>f', dataStr[0:4])
+            mag_covariance = value[0]
+            print 'Mag covariance is %f' % mag_covariance
+        elif command == PROCESS_COVARIANCE_REPORT:
+            value = struct.unpack('>f', dataStr[0:4])
+            process_covariance = value[0]
+            print 'Process covariance is %f' % process_covariance
+        elif command == STATE_COVARIANCE_REPORT:
+            value = struct.unpack('>f', dataStr[0:4])
+            mat00 = value[0]
+            value = struct.unpack('>f', dataStr[4:8])
+            mat01 = value[0]
+            value = struct.unpack('>f', dataStr[8:12])
+            mat02 = value[0]
+            value = struct.unpack('>f', dataStr[12:16])
+            mat10 = value[0]
+            value = struct.unpack('>f', dataStr[16:20])
+            mat11 = value[0]
+            value = struct.unpack('>f', dataStr[20:24])
+            mat12 = value[0]
+            value = struct.unpack('>f', dataStr[24:28])
+            mat20 = value[0]
+            value = struct.unpack('>f', dataStr[28:32])
+            mat21 = value[0]
+            value = struct.unpack('>f', dataStr[32:36])
+            mat22 = value[0]
+            print 'State covariance is [%f %f %f | %f %f %f | %f %f %f]' % (mat00, mat01, mat02, mat10, mat11, mat12, mat20, mat21, mat22)
+        elif command == EKF_CONFIG_REPORT:
+            if data[0] & 0x02:
+                print 'Accelerometer updates ARE used for pitch and roll angle correction'
+            else:
+                print 'Accelerometer updates ARE NOT used for pitch and roll angle correction'
+            
+            if data[0] & 0x01:
+                print 'Magnetometer updates ARE used for yaw angle correction'
+            else:
+                print 'Magnetometer updates ARE NOT used for yaw angle correction'
+        elif command == GYRO_ALIGNMENT_REPORT:
+            pass
+        elif command == ACCEL_ALIGNMENT_REPORT:
+            pass
+        elif command == MAG_REF_VECTOR_REPORT:
+            pass
+        elif command == MAG_CAL_REPORT:
+            pass
+        elif command == MAG_BIAS_REPORT:
+            pass
+        elif command == BROADCAST_MODE_REPORT:
+            pass
         else:
-            print "Packet is %d" % command
+            print "Unsupported packet %d" % command
 
     def close(self):
         """
@@ -290,20 +400,6 @@ class CHR6dmIMU(object):
             self.ser.flushInput()
             self.ser.flushOutput()
             self.ser.close()
-
-    def handle_packet(self, packet_type, data_size, data):
-        if packet_type == COMMAND_COMPLETE:
-            print 'command complete'
-        elif packet_type == COMMAND_FAILED:
-            print 'command failed'
-        elif packet_type == BAD_CHECKSUM:
-            print 'bad checksum'
-        elif packet_type == BAD_DATA_LENGTH:
-            print 'bad data length'
-        elif packet_type == UNRECOGNIZED_PACKET:
-            print 'unrecognized packet'
-        elif packet_type == BUFFER_OVERFLOW:
-            print 'buffer overflow'
 
     def set_active_channels(self, channels):
         """
@@ -318,7 +414,7 @@ class CHR6dmIMU(object):
         high_byte += '1' if channels['roll_rate'] else '0'
         high_byte += '1' if channels['mx'] else '0'
         high_byte += '1' if channels['my'] else '0'
-
+        
         low_byte = str()
         low_byte += '1' if channels['mz'] else '0'
         low_byte += '1' if channels['gx'] else '0'
@@ -328,17 +424,15 @@ class CHR6dmIMU(object):
         low_byte += '1' if channels['ay'] else '0'
         low_byte += '1' if channels['az'] else '0'
         low_byte += '0'
-
+        
         high_byte = int(high_byte, 2)
         low_byte = int(low_byte, 2)
-
+        
         self.write_to_imu(SET_ACTIVE_CHANNELS, (high_byte, low_byte))
-        self.read_from_imu()
 
     def set_silent_mode(self):
         print "Setting silent mode"
         self.write_to_imu(SET_SILENT_MODE)
-        self.read_from_imu()
 
     def set_broadcast_mode(self, hz):
         x = int((hz - 20) / (280.0/255.0))
@@ -349,11 +443,9 @@ class CHR6dmIMU(object):
 
     def get_data(self):
         self.write_to_imu(GET_DATA)
-        self.read_from_imu()
 
     def get_active_channels(self):
         self.write_to_imu(GET_ACTIVE_CHANNELS)
-        self.read_from_imu()
         
     def enable_accel_angrate_orientation(self):
         ch = {'yaw':1, 'pitch':1, 'roll':1,
@@ -369,7 +461,7 @@ class CHR6dmIMU(object):
         while self.ser.read() != 's':
             print "looking for packet prefix 'snp'"
         packet = 's' + self.ser.read(2) # read 'n' and 'p'
-
+        
         if packet != 'snp':
             print "Received corrupted packet, prefix was %s" % packet
             return
@@ -390,8 +482,7 @@ class CHR6dmIMU(object):
         chkSumData = map(int, chkSumData, [16] * len(chkSumData))
         chkSumRx = (chkSumData[0] << 8) | chkSumData[1]
 
-        chkSum = reduce(int.__add__, map(ord, ['s', 'n', 'p']), 0)
-        chkSum = chkSum + command + len(data) + reduce(int.__add__, data, 0)
+        chkSum = self.calculate_checksum(command, data)
         
         if chkSumRx != chkSum:
             print "Checksums don't match %d != %d" % (chkSumRx, chkSum)
@@ -480,8 +571,8 @@ class CHR6dmIMU(object):
 
 if __name__ == "__main__":
     sio = CHR6dmIMU()
-    #sio.set_silent_mode()
-    sio.set_broadcast_mode(100)
+    sio.set_silent_mode()
+    #sio.set_broadcast_mode(100)
     
     ch = {'yaw':1, 'pitch':1, 'roll':1,
           'yaw_rate':1, 'pitch_rate':1, 'roll_rate':1,
@@ -489,9 +580,9 @@ if __name__ == "__main__":
           'gx':1, 'gy':1, 'gz':1,
           'ax':1, 'ay':1, 'az':1,}
     
-    #sio.set_active_channels(ch)
+    sio.set_active_channels(ch)
     #sio.get_active_channels()
-    while True:
-        sio.get_data()
+    #while True:
+    #    sio.get_data()
         #time.sleep(0.2)
 
