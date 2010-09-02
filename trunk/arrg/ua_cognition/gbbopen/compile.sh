6529ac94ca6a016bf2bda49f7280aaf6d3337136
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-09-02 16:19:51 +0000

    Fixes to makefile to reflect new asd, avoid some unnecessary recompilation

--- trunk/arrg/ua_cognition/gbbopen/compile.sh
+++ trunk/arrg/ua_cognition/gbbopen/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ROSLISP_CURRENT_PACKAGE=gbbopen sbcl --noinform --end-runtime-options --noprint --no-userinit --disable-debugger --load `(rospack find gbbopen)`/load-gbbopen.lisp --end-toplevel-options $*
+ROSLISP_CURRENT_PACKAGE=gbbopen `(rospack find sbcl)`/scripts/run-sbcl.sh --noinform --end-runtime-options --noprint --no-userinit --disable-debugger --load `(rospack find gbbopen)`/load-gbbopen.lisp --end-toplevel-options $*
