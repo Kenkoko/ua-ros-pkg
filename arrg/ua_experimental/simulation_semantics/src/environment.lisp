9fb1b61c67032c5ce834e90e027da173279c8aef
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-10-30 03:01:43 +0000

    Added wubble_world2d wrapper package, and a test case for it to simulation_semantics

--- arrg/ua_experimental/simulation_semantics/src/environment.lisp
+++ arrg/ua_experimental/simulation_semantics/src/environment.lisp
@@ -2,12 +2,12 @@
 
 (defun initialize (object-vector)
   (oomdp_msgs-srv:start_state-val
-   (call-service "environment/initialize" 'oomdp_msgs-srv:InitializeEnvironment
+   (call-service "verb_learning/initialize_environment" 'oomdp_msgs-srv:InitializeEnvironment
                  :object_states object-vector)))
 
 (defun perform-action (action)
   "Performs action (a string) and returns the resulting state."
   (oomdp_msgs-srv:new_state-val 
-   (call-service "environment/perform_action" 
+   (call-service "verb_learning/perform_action" 
                  'oomdp_msgs-srv:PerformAction
                  :action action)))
