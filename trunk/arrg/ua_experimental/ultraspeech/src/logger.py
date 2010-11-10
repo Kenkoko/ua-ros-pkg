6c17f256c6485788f33a80fd3a28728afa45c56f
jeffreyjberry@gmail.com <jeffreyjberry@gmail.com@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-11-10 03:45:21 +0000

    working monocam interaction with subjectUI and logger

--- trunk/arrg/ua_experimental/ultraspeech/src/logger.py
+++ trunk/arrg/ua_experimental/ultraspeech/src/logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os, sys
 
@@ -6,26 +7,55 @@ import roslib
 roslib.load_manifest('ultraspeech')
 import rospy
 
-from ultraspeech.msg import Control, CurrentStim
+from ultraspeech.msg import Control, CurrentStim, SaveFile
+from sensor_msgs.msg import CameraInfo
 
 class Logger:
     def __init__(self):
         self.logfile = None
+        self.runstatus = 0
         rospy.init_node("logger", anonymous=True)
         rospy.Subscriber("control", Control, self.control_cb)
+        rospy.Subscriber("current_stimulus", CurrentStim, self.currentstim_cb)
+        rospy.Subscriber("monocam/camera_info", CameraInfo, self.monocaminfo_cb)
+        rospy.Subscriber("monocam/monocam_capture/save_name", SaveFile, self.monocamsave_cb)
         rospy.spin()
         
     def control_cb(self, data):
-        timestamp = str(data.header.stamp.secs) + '.' + str(data.header.stamp.nsecs)
-        runstatus = data.run
+	timestamp = self.getTime(str(data.header.stamp.secs), str(data.header.stamp.nsecs))	
+        self.runstatus = data.run
         directory = data.directory
         if self.logfile == None:
             logfilename = directory + '/logfile.txt'
             self.logfile = open(logfilename, 'w')
-        self.logfile.write('control\ttime:%s\trun:%d\tdirectory:%s\n' %(timestamp, runstatus, directory))    
-        
-        
- 
+        self.logfile.write('topic:control,time:%s,run:%d,directory:%s\n' %(timestamp, self.runstatus, directory))    
+
+    def currentstim_cb(self, data):
+	timestamp = self.getTime(str(data.header.stamp.secs), str(data.header.stamp.nsecs))	
+	stim = data.stimulus
+	rep = data.rep
+	batch = data.batch
+	self.logfile.write('topic:currentstim,time:%s,stimulus:%s,rep:%d,batch:%d\n' %(timestamp, stim, rep, batch))
        
+    def monocaminfo_cb(self, data):
+        if self.runstatus == 1:
+	    timestamp = self.getTime(str(data.header.stamp.secs), str(data.header.stamp.nsecs))
+	    frame = data.header.seq
+	    self.logfile.write('topic:monocaminfo,time:%s,frame:%d\n' %(timestamp, frame))
+	else:
+	  pass
+
+    def monocamsave_cb(self, data):
+	timestamp = self.getTime(str(data.header.stamp.secs), str(data.header.stamp.nsecs))
+	filename = data.filepath
+	self.logfile.write('topic:monocamsave,time:%s,filepath:%s\n' %(timestamp, filename))
+	  
+    def getTime(self, secs, nsecs):
+	if len(nsecs) < 9:
+	    while (len(nsecs) != 9):
+		nsecs = '0' + nsecs
+	return secs + '.' + nsecs
+        
+        
 if __name__ == "__main__":
     Logger()
