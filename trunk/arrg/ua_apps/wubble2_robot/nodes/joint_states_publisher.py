450d21323a5a2def2f0f697114187d2c0f56b28d
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-12-07 01:01:58 +0000

    Add wubble2 specific nodes and launch scripts.

---
+++ trunk/arrg/ua_apps/wubble2_robot/nodes/joint_states_publisher.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2010, Arizona Robotics Research Group, University of Arizona
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.  * Redistributions
+#     in binary form must reproduce the above copyright notice, this list of
+#     conditions and the following disclaimer in the documentation and/or
+#     other materials provided with the distribution. # Neither the name of
+#     the Willow Garage, Inc. nor the names of its contributors may be used to
+#     endorse or promote products derived from this software without specific
+#     prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib; roslib.load_manifest('wubble2_robot')
+import rospy
+
+from sensor_msgs.msg import JointState as JointStatePR2
+from ua_controller_msgs.msg import JointState as JointStateWubble
+
+class JointStateMessage():
+    def __init__(self, name, position, velocity, effort):
+        self.name = name
+        self.position = position
+        self.velocity = velocity
+        self.effort = effort
+
+class JointStatesPublisher():
+    def __init__(self):
+        rospy.init_node('wubble_joint_states_publisher', anonymous=True)
+        
+        self.joint_states = {'base_caster_support_joint': JointStateMessage('base_caster_support_joint', 0.0, 0.0, 0.0),
+                             'caster_wheel_joint': JointStateMessage('caster_wheel_joint', 0.0, 0.0, 0.0),
+                             'base_link_left_wheel_joint': JointStateMessage('base_link_left_wheel_joint', 0.0, 0.0, 0.0),
+                             'base_link_right_wheel_joint': JointStateMessage('base_link_right_wheel_joint', 0.0, 0.0, 0.0)}
+                             
+        self.controllers = ('shoulder_pitch_controller',
+                            'shoulder_yaw_controller',
+                            'shoulder_roll_controller',
+                            'elbow_pitch_controller',
+                            'wrist_roll_controller',
+                            'wrist_pitch_controller',
+                            'wrist_yaw_controller',
+                            'gripper_left_finger_controller',
+                            'gripper_right_finger_controller',
+                            'head_pan_controller',
+                            'head_tilt_controller',
+                            'neck_tilt_controller')
+                            
+        # Start controller state subscribers
+        [rospy.Subscriber(c + '/state', JointStateWubble, self.controller_state_handler) for c in self.controllers]
+        
+        # Start publisher
+        self.joint_states_pub = rospy.Publisher('joint_states', JointStatePR2)
+        
+        publish_rate = rospy.get_param('~rate', 50)
+        r = rospy.Rate(publish_rate)
+        
+        while not rospy.is_shutdown():
+            self.publish_joint_states()
+            r.sleep()
+            
+    def controller_state_handler(self, msg):
+        jcs = JointStateMessage(msg.name, msg.current_pos, msg.velocity, msg.load)
+        self.joint_states[msg.name] = jcs
+        
+    def publish_joint_states(self):
+        # Construct message & publish joint states
+        msg = JointStatePR2()
+        msg.name = []
+        msg.position = []
+        msg.velocity = []
+        msg.effort = []
+        
+        for joint in self.joint_states.values():
+            msg.name.append(joint.name)
+            msg.position.append(joint.position)
+            msg.velocity.append(joint.velocity)
+            msg.effort.append(joint.effort)
+            
+        msg.header.stamp = rospy.Time.now()
+        self.joint_states_pub.publish(msg)
+
+if __name__ == '__main__':
+    try:
+        s = JointStatesPublisher()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
+
---
+++ trunk/arrg/ua_apps/wubble2_robot/scripts/turn_torque_on_off.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2010, Arizona Robotics Research Group,
+# University of Arizona. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of University of Arizona nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Antons Rebguns
+#
+
+from __future__ import division
+
+from math import fabs
+from math import fmod
+from math import pi
+
+from threading import Thread
+
+from scipy.interpolate import splrep, spalde
+
+import roslib
+roslib.load_manifest('wubble2_robot')
+
+import rospy
+from ax12_controller_core.srv import TorqueEnable
+
+
+if __name__ == '__main__':
+    rospy.init_node('torque_on_off')
+    
+    joint_controllers = ['shoulder_pitch_controller',
+                         'shoulder_yaw_controller',
+                         'shoulder_roll_controller',
+                         'elbow_pitch_controller',
+                         'wrist_roll_controller',
+                         'wrist_pitch_controller',
+                         'wrist_yaw_controller',
+                         'gripper_left_finger_controller',
+                         'gripper_right_finger_controller']
+                         
+    joint_torque_srvs = [rospy.ServiceProxy(controller + '/torque_enable', TorqueEnable) for controller in joint_controllers]
+    
+    for srv in joint_torque_srvs:
+        srv(True)
+
