90956f853f892d37b345734c79bd3420096f3808
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-12-14 09:21:59 +0000

    Roll in Andrew's changes to fft node.

--- arrg/ua_experimental/ua_audio_fft/src/audio_fft.py
+++ arrg/ua_experimental/ua_audio_fft/src/audio_fft.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('ua_audio_fft')
+import roslib; roslib.load_manifest('ua_audio_transform')
 import rospy
-import threading
 import sys
+import traceback
+import getopt
 from scipy.fftpack import fft, fftshift
-
-import matplotlib
-matplotlib.use('WXAgg')
-from pylab import *
 
 from ua_audio_msgs.msg import AudioRawStream
 from ua_audio_msgs.msg import TransformedStream
 
 class audio_fft(object):
 
-    def __init__(self):
-        rospy.init_node('audio_fft', anonymous=True)
-        self.lock = threading.Lock()
+    def __init__(self,argv):
+        rospy.init_node('audio_transform', anonymous=True)
         self.sub = rospy.Subscriber("audio_capture/audio", AudioRawStream, self.callback)
-        self.pub = rospy.Publisher("fft_audio",TransformedStream)
-
-        parse_cline(sys.argv[1:])
-
+        self.pub = rospy.Publisher("transform_audio",TransformedStream)
+        self.parse_cline(argv)
         self.first_time = True
-        self.numSamples = 0
 
-    def parse_cline(argv):
+    def parse_cline(self,argv):
         self.wszp = 0.25
         self.ovrp = 0.125
         self.pwr = True
         try:
             opts, args = getopt.getopt(argv, "w:o:p:", ["window-size=","overlap=","powers_of_two="])
         except:
+            traceback.print_exc()
+            rospy.loginfo(rospy.get_name()+": Command line fail")
             sys.exit(2)
         for opt, arg in opts:
             if opt in ("-w","--window-size"):
+                rospy.loginfo(rospy.get_name()+": window-size="+arg)
                 self.wszp = float(arg)
             elif opt in ("-o","--overlap"):
+                rospy.loginfo(rospy.get_name()+": overlap="+arg)
                 self.ovrp = float(arg)
             elif opt in ("-p","--powers_of_two"):
-                self.pwr = bool(arg)
+                if arg=="False":
+                    self.pwr = False
+                rospy.loginfo(rospy.get_name()+": powers_of_two="+str(self.pwr))
         if self.wszp <= 0:
             self.wszp = 0.25
         if self.ovrp <= 0:
@@ -47,50 +46,58 @@ class audio_fft(object):
         if self.ovrp > self.wszp:
             self.ovrp = self.wszp
 
-    def init_freq(data):
+    def init_freq(self,data):
         self.first_time = False
-        self.
+        self.wsz = int(data.sample_rate * self.wszp)
+        self.ovr = int(data.sample_rate * self.ovrp)
+        if self.pwr:
+            lo_pwr = 1
+            hi_pwr = 2
+            while hi_pwr < self.wsz:
+                lo_pwr = hi_pwr
+                hi_pwr = hi_pwr << 1
+            if self.wsz - lo_pwr < hi_pwr - self.wsz:
+                self.wsz = lo_pwr
+            else:
+                self.wsz = hi_pwr
+            lo_pwr = 1
+            hi_pwr = 2
+            while hi_pwr < self.ovr:
+                lo_pwr = hi_pwr
+                hi_pwr = hi_pwr << 1
+            if self.ovr - lo_pwr < hi_pwr - self.ovr:
+                self.ovr = lo_pwr
+            else:
+                self.ovr = hi_pwr
+        rospy.loginfo(rospy.get_name()+": From frequency %d, using window-size %d and overlap %d.",data.sample_rate,self.wsz,self.ovr)
+        self.nUniquePts = (self.wsz+1)/2 + (self.wsz+1)%2
 
     def callback(self,data):
-        self.lock.acquire()
         
         if(self.first_time):
-            init_freq(data)
-
-
-
-
-        if self.numSamples == 0:
             self.data = data.samples
+            self.init_freq(data)
         else:
             self.data = self.data + data.samples
 
-        self.numSamples = self.numSamples + 1
-
-        if self.numSamples == self.wsz:
-            n = len(self.data) 
-            p = fft(self.data) # take the fourier transform 
-            nUniquePts = ceil((n+1)/2.0)
-            p = p[0:nUniquePts]
+        while len(self.data) >= self.wsz:
+            p = fft(self.data[0:self.wsz-1]) # take the fourier transform 
+            p = p[0:self.nUniquePts]
             p = abs(p)
-            p = p / float(n) # scale by the number of points so that
+            p = p / float(self.wsz) # scale by the number of points so that
                              # the magnitude does not depend on the length 
                              # of the signal or on its sampling frequency  
             p = p**2  # square it to get the power 
             # odd nfft excludes Nyquist point
-            if n % 2 > 0: # we've got odd number of points fft
+            if self.wsz % 2 > 0: # we've got odd number of points fft
                 p[1:len(p)] = p[1:len(p)] * 2
             else:
                 p[1:len(p) -1] = p[1:len(p) - 1] * 2 # we've got even number of points fft
             rospy.loginfo(rospy.get_name()+": publishing %d datapoints.", len(p))
-            self.data = self.data[(len(data.samples)*self.ovr):len(self.data)]
-            self.numSamples = self.numSamples - self.ovr
-            self.lock.release()
-            self.pub.publish(p,data.num_channels,data.sample_rate,nUniquePts,self.wsz,self.ovr)
-        else:
-            self.lock.release()
-
+            self.data = self.data[self.ovr:len(self.data)]
+            self.pub.publish(p,1,data.num_channels,data.sample_rate,self.nUniquePts,self.wsz,self.ovr)
+            #Currently the '1' indicates that it is fft transform. Other transforms can be indicated by other integers.
 
 if __name__ == '__main__':
-    A = audio_fft()
+    A = audio_fft(sys.argv[1:])
     rospy.spin()
