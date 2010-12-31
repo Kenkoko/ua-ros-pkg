3e6cb38c641d04b0290fed0ca78d418865b1a035
deerslyr1 <deerslyr1@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2010-12-31 23:47:46 +0000

    Updated the synthetic data generator to deal with covariance.

--- arrg/ua_experimental/time_series/scripts/sim.R
+++ arrg/ua_experimental/time_series/scripts/sim.R
@@ -29,17 +29,17 @@ create.episode <- function(ep.length,mn.vec,cov.mat,cut.points){
 args <- commandArgs(TRUE)
 
 # initial arguments
-#   class - 1 or 2 depending on which class you want to generate
 #   prefix - where you want the file written
 #   p - the number of streams.
-#   alphabet.size - the number of symbols (constant in each stream)
 #   episode.length -- the length of each episode
-class.index <- args[1]
-prefix <- args[2]
-p <- as.integer(args[3])
-mean <- as.real(args[4])
-means <- rep(mean, p)
-episode.length <- as.integer(args[5])
+#   mean - the mean value to shift the middle episode
+#   cov.pct -- the amount to modify the covariance for the middle episode (0 - 1)
+prefix <- args[1]              # File location with class label
+p <- as.integer(args[2])       # Number of streams
+ep.len <- as.integer(args[3])  # Length of individual episodes
+mean <- as.real(args[4])       # Mean value to shift the middle episode
+means <- rep(mean, p)          
+cov.pct <- as.real(args[5])    # Amount to modify covariance for middle episode (0-1)
 
 ntrain <- 60
 alphabet.size <- 7
@@ -52,52 +52,34 @@ cut.points <- qnorm(seq(0.05,0.95,len=alphabet.size+1))
 # create some data
 # this is the number of training instances
 # fixed for both classes (so we have P(C_1)=P(C_2))
+cov.mat.A <- diag(1/50,p)
+cov.mat.B <- diag(1/10,p)
+cov.mat.B[2:5,2:5] <- 0.5
 
-if (class.index == 1) { 
-  # this writes "ntrain" files for class 1 (labelled "f")
-  # all the work is done by the function "create.episode"
-  for (i in 1:ntrain){
-    c1 <- NULL
-    
-    for (j in 1:n.episodes) {
-    	c1 <- rbind(c1,create.episode(episode.length,rep(0,p),diag(1/50,p),cut.points))
-    }
-    write.table(c1,paste(prefix,"f",i,sep=""),row=F,col=F)
+print(ep.len)	
+print(means)
+
+for (i in 1:ntrain) {
+  
+  c2 <- NULL
+  
+  # Normal episodes.
+  for (j in 1:floor(n.episodes/2)) {
+    c2 <- rbind(c2,create.episode(ep.len,rep(0,p),diag(1/50,p),cut.points))
   }
-} else {
-  # create class 2
-  # now create "ntrain" files for class 2 (labelled "g")
-  # here we construct the class with 5 calls of "create.episode"
-  # with different parameters at each call
-  # (This can be generalised considerably - random lengths,
-  # random parameters etc)
 
-  # below, only the third episode is different - showing a
-  #different covriance stucture
-
-  print(episode.length)	
-  print(means)
-
-  for (i in 1:ntrain){
+  # Change the structure of this episode.  
+  # change in covariance
+  cov.mat <- cov.mat.A + (cov.pct * (cov.mat.B - cov.mat.A))
+  c2 <- rbind(c2,create.episode(ep.len,means,cov.mat,cut.points))
   
-    c2 <- NULL
-  
-    for (j in 1:floor(n.episodes/2)) {
-      c2 <- rbind(c2,create.episode(episode.length,rep(0,p),diag(1/50,p),cut.points))
-    }
-  
-    # change in covariance
-#    cov.mat <- diag(1/10,p)
-#    cov.mat[2:5,2:5] <- 1/2
-	cov.mat <- diag(1/50,p)
-    c2 <- rbind(c2,create.episode(episode.length,means,cov.mat,cut.points))
 
-    for (j in 1:floor(n.episodes/2)) {
-      c2 <- rbind(c2,create.episode(episode.length,rep(0,p),diag(1/50,p),cut.points))
-    }
-
-    write.table(c2,paste(prefix,"g",i,sep=""),row=F,col=F)
+  #Normal episodes.  
+  for (j in 1:floor(n.episodes/2)) {
+    c2 <- rbind(c2,create.episode(ep.len,rep(0,p),diag(1/50,p),cut.points))
   }
+
+  write.table(c2,paste(prefix,i,sep=""),row=F,col=F)
 }
 
 
