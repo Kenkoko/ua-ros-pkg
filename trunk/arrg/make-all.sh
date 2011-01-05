4055dd733ac3a8cd555eb946c519fc1f9be3b0aa
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-01-05 00:14:14 +0000

    point to correct erratic stack.

--- trunk/arrg/make-all.sh
+++ trunk/arrg/make-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rosmake $1 --rosdep-install --rosdep-yes --skip-blacklist ua_cognition ua_drivers ua_vision ua_apps ua_controllers ua_robots wubble_world ua_experimental ua_language wubble_navigation wubble_navigation_apps videre_erratic
+rosmake $1 --rosdep-install --rosdep-yes --skip-blacklist ua_cognition ua_drivers ua_vision ua_apps ua_controllers ua_robots wubble_world ua_experimental ua_language wubble_navigation wubble_navigation_apps erratic_robot
 
