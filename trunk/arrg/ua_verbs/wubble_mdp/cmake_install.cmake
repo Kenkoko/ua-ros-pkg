d1ac22f91535aa30ad566aa71636121ab48fb6df
dhewlett <dhewlett@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-01-25 00:53:44 +0000

    Fixed bug where locations could very rarely change position

--- trunk/arrg/ua_verbs/wubble_mdp/cmake_install.cmake
+++ trunk/arrg/ua_verbs/wubble_mdp/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dhewlett/ros/ua-ros-pkg/ua_experimental/wubble_mdp
+# Install script for directory: /home/dhewlett/ros/ua-ros-pkg/ua_verbs/wubble_mdp
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/dhewlett/ros/ua-ros-pkg/ua_experimental/wubble_mdp/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/dhewlett/ros/ua-ros-pkg/ua_verbs/wubble_mdp/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/dhewlett/ros/ua-ros-pkg/ua_experimental/wubble_mdp/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/dhewlett/ros/ua-ros-pkg/ua_verbs/wubble_mdp/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
