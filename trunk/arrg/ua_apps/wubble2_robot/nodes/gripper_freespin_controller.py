d6f7db1bc04078afa0ddf014342a9e5c57728a2b
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-02-04 20:36:02 +0000

    Monitor joint temperatures and adjust torque accordingly so as to not overheat the motors

--- trunk/arrg/ua_apps/wubble2_robot/nodes/gripper_freespin_controller.py
+++ trunk/arrg/ua_apps/wubble2_robot/nodes/gripper_freespin_controller.py
@@ -33,6 +33,7 @@
 import roslib; roslib.load_manifest('wubble2_robot')
 import rospy
 
+from ax12_driver_core.ax12_const import DMXL_MAX_SPEED_RAD
 from ax12_controller_core.srv import SetSpeed
 from ax12_controller_core.srv import SetTorqueLimit
 from ua_controller_msgs.msg import JointState
@@ -42,6 +43,8 @@ import actionlib
 from std_msgs.msg import Float64
 from wubble2_robot.msg import WubbleGripperAction
 from wubble2_robot.msg import WubbleGripperGoal
+
+from threading import Thread
 
 class GripperActionController():
     def __init__(self):
@@ -80,9 +83,49 @@ class GripperActionController():
         rospy.Subscriber('/interface_kit/106950/sensor/7', Float64Stamped, self.process_ir_sensor)
         self.ir_distance = 0.0
         
+        # Temperature monitor thread
+        Thread(target=self.monitor_temperature).start()
+        
         self.action_server = actionlib.SimpleActionServer('wubble_gripper_action', WubbleGripperAction, execute_cb=self.process_gripper_action)
         
         rospy.loginfo('gripper_freespin_controller: ready to accept goals')
+
+    def monitor_temperature(self):
+        rospy.wait_for_message(self.left_finger_controller + '/state', JointState)
+        rospy.wait_for_message(self.right_finger_controller + '/state', JointState)
+        rospy.loginfo('Gripper temperature monitor thread started  successfully')
+        pre_overheat_trigger = False
+        overheat_trigger = False
+        
+        r = rospy.Rate(500)
+        while not rospy.is_shutdown():
+            l_temp = self.left_finger_joint_state.motor_temps[0]
+            r_temp = self.right_finger_joint_state.motor_temps[0]
+            
+            # halve the current torque to get temps under control
+            l_goal = self.left_finger_joint_state.goal_pos * 0.5
+            r_goal = self.right_finger_joint_state.goal_pos * 0.5
+            
+            if l_temp >= 63 or r_temp >= 63:
+                if not pre_overheat_trigger:
+                    rospy.logwarn('Gripper motors are overheating [LM: %dC, RM: %dC]' % (l_temp, r_temp))
+                    rospy.logwarn('Setting torque to [LM: %.2f, RM: %.2f]' % (l_goal, r_goal))
+                    self.left_finger_joint_position_pub.publish(l_goal)
+                    self.right_finger_joint_position_pub.publish(r_goal)
+                    pre_overheat_trigger = True
+            else:
+                pre_overheat_trigger = False
+                
+            if l_temp >= 66 or r_temp >= 66:
+                if not overheat_trigger:
+                    rospy.logwarn('Disabling gripper motors torque [LM: %dC, RM: %dC]' % (l_temp, r_temp))
+                    self.left_finger_joint_position_pub.publish(-0.5)
+                    self.right_finger_joint_position_pub.publish(0.5)
+                    overheat_trigger = True
+            else:
+                overheat_trigger = False
+                
+            r.sleep()
 
     def process_left_finger_joint_state(self, msg):
         self.left_finger_joint_state = msg
@@ -106,28 +149,28 @@ class GripperActionController():
         r = rospy.Rate(500)
         
         if req.command == WubbleGripperGoal.CLOSE_GRIPPER:
-            self.left_finger_joint_position_pub.publish(-4.0)
-            self.right_finger_joint_position_pub.publish(4.0)
+            desired_torque = req.torque_limit * DMXL_MAX_SPEED_RAD
+            self.left_finger_joint_position_pub.publish(-2.0)
+            self.right_finger_joint_position_pub.publish(2.0)
             rospy.sleep(0.1)
             
             while self.left_finger_joint_state.is_moving or self.right_finger_joint_state.is_moving:
                 r.sleep()
                 
-            self.left_finger_joint_position_pub.publish(self.left_finger_joint_state.current_pos - 10.0)
-            self.right_finger_joint_position_pub.publish(self.right_finger_joint_state.current_pos + 10.0)
+            self.left_finger_joint_position_pub.publish(-desired_torque)
+            self.right_finger_joint_position_pub.publish(desired_torque)
             
             self.action_server.set_succeeded()
         elif req.command == WubbleGripperGoal.OPEN_GRIPPER:
-            self.left_finger_joint_torque_limit_srv(req.torque_limit)
-            self.right_finger_joint_torque_limit_srv(req.torque_limit)
-            self.left_finger_joint_position_pub.publish(4.0)
-            self.right_finger_joint_position_pub.publish(-4.0)
+            desired_torque = req.torque_limit * DMXL_MAX_SPEED_RAD
+            self.left_finger_joint_position_pub.publish(2.0)
+            self.right_finger_joint_position_pub.publish(-2.0)
             
             while self.left_finger_joint_state.current_pos < 0.8 or self.right_finger_joint_state.current_pos > -0.8:
                 r.sleep()
                 
-            self.left_finger_joint_position_pub.publish(0.0)
-            self.right_finger_joint_position_pub.publish(0.0)
+            self.left_finger_joint_position_pub.publish(0.5)
+            self.right_finger_joint_position_pub.publish(0.5)
             self.action_server.set_succeeded()
         else:
             rospy.logerr('Unrecognized command, gripper is not moving enywhere!')
