6cbbe8954eac889d1bd94af09a494a53d07d46a1
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-02-08 23:51:54 +0000

    New gripper controller code

--- trunk/arrg/ua_apps/wubble2_robot/nodes/gripper_freespin_controller.py
+++ trunk/arrg/ua_apps/wubble2_robot/nodes/gripper_freespin_controller.py
@@ -34,6 +34,7 @@ import roslib; roslib.load_manifest('wubble2_robot')
 import rospy
 
 from ax12_driver_core.ax12_const import DMXL_MAX_SPEED_RAD
+from ax12_driver_core.ax12_const import DMXL_SPEED_RAD_SEC_PER_TICK
 from ax12_controller_core.srv import SetSpeed
 from ax12_controller_core.srv import SetTorqueLimit
 from ua_controller_msgs.msg import JointState
@@ -56,49 +57,63 @@ class GripperActionController():
         self.left_finger_joint = rospy.get_param(self.left_finger_controller + '/joint_name')
         self.right_finger_joint = rospy.get_param(self.right_finger_controller + '/joint_name')
         
-        self.left_finger_joint_state = None
-        self.right_finger_joint_state = None
-        
         # Publishers and Subscribers for all gripper joint controllers
-        self.left_finger_joint_position_pub = rospy.Publisher(self.left_finger_controller + '/command', Float64)
-        self.right_finger_joint_position_pub = rospy.Publisher(self.right_finger_controller + '/command', Float64)
+        self.left_finger_joint_torque_pub = rospy.Publisher(self.left_finger_controller + '/command', Float64)
+        self.right_finger_joint_torque_pub = rospy.Publisher(self.right_finger_controller + '/command', Float64)
         
         self.left_finger_joint_state_sub = rospy.Subscriber(self.left_finger_controller + '/state', JointState, self.process_left_finger_joint_state)
         self.right_finger_joint_state_sub = rospy.Subscriber(self.right_finger_controller + '/state', JointState, self.process_right_finger_joint_state)
-        
-        # Services
-        self.left_finger_joint_velocity_srv = rospy.ServiceProxy(self.left_finger_controller + '/set_speed', SetSpeed)
-        self.right_finger_joint_velocity_srv = rospy.ServiceProxy(self.right_finger_controller + '/set_speed', SetSpeed)
-        self.left_finger_joint_torque_limit_srv = rospy.ServiceProxy(self.left_finger_controller + '/set_torque_limit', SetTorqueLimit)
-        self.right_finger_joint_torque_limit_srv = rospy.ServiceProxy(self.right_finger_controller + '/set_torque_limit', SetTorqueLimit)
         
         # Pressure sensors
         # 0-3 - left finger
         # 4-7 - right finger
         num_sensors = 8
-        [rospy.Subscriber('/interface_kit/124427/sensor/%d' % i, Float64Stamped, self.process_pressure_sensors, i) for i in range(num_sensors)]
         self.pressure = [0.0] * num_sensors
+        [rospy.Subscriber('/interface_kit/124427/sensor/%d' % i, Float64Stamped, self.process_pressure_sensors, i) for i in range(num_sensors)]
+        
+        # pressure sensors are at these values when no external pressure is applied
+        self.pressure_init = [0.0, 0.0, 163.0, 0.0,
+                              0.0, 0.0, 305.0, 0.0]
+        self.total_pressure_pub = rospy.Publisher('total_pressure', Float64)
+        self.close_gripper = False
+        self.overheating = False
+        self.dynamic_torque_control = False
+        self.lower_pressure = 800.0
+        self.upper_pressure = 1000.0
         
         # IR sensor
         rospy.Subscriber('/interface_kit/106950/sensor/7', Float64Stamped, self.process_ir_sensor)
         self.ir_distance = 0.0
         
-        # Temperature monitor thread
-        Thread(target=self.monitor_temperature).start()
+        # Make sure we receive current joint states before starting threads
+        rospy.loginfo('Waiting to receive gripper joints state...')
+        self.left_finger_joint_state = rospy.wait_for_message(self.left_finger_controller + '/state', JointState)
+        self.right_finger_joint_state = rospy.wait_for_message(self.right_finger_controller + '/state', JointState)
+        
+        # Temperature monitor and torque control thread
+        Thread(target=self.gripper_monitor).start()
         
         self.action_server = actionlib.SimpleActionServer('wubble_gripper_action', WubbleGripperAction, execute_cb=self.process_gripper_action)
         
         rospy.loginfo('gripper_freespin_controller: ready to accept goals')
 
-    def monitor_temperature(self):
-        rospy.wait_for_message(self.left_finger_controller + '/state', JointState)
-        rospy.wait_for_message(self.right_finger_controller + '/state', JointState)
-        rospy.loginfo('Gripper temperature monitor thread started  successfully')
+
+    def within_tolerance(self, a, b, tolerance):
+        return abs(a - b) < tolerance
+
+
+    def gripper_monitor(self):
+        rospy.loginfo('Gripper temperature monitor and torque control thread started successfully')
         pre_overheat_trigger = False
         overheat_trigger = False
-        
+        max_pressure = 8000.0
         r = rospy.Rate(500)
+        
         while not rospy.is_shutdown():
+            pressure = max(0.0, sum(self.pressure) - sum(self.pressure_init))
+            self.total_pressure_pub.publish(pressure)
+            
+            #----------------------- TEMPERATURE MONITOR ---------------------------#
             l_temp = self.left_finger_joint_state.motor_temps[0]
             r_temp = self.right_finger_joint_state.motor_temps[0]
             
@@ -106,35 +121,73 @@ class GripperActionController():
             l_goal = self.left_finger_joint_state.goal_pos * 0.5
             r_goal = self.right_finger_joint_state.goal_pos * 0.5
             
-            if l_temp >= 63 or r_temp >= 63:
-                if not pre_overheat_trigger:
+            if l_temp >= 76 or r_temp >= 76:
+                if not overheat_trigger:
+                    rospy.logwarn('Disabling gripper motors torque [LM: %dC, RM: %dC]' % (l_temp, r_temp))
+                    self.left_finger_joint_torque_pub.publish(-0.5)
+                    self.right_finger_joint_torque_pub.publish(0.5)
+                    overheat_trigger = True
+            elif l_temp >= 73 or r_temp >= 73:
+                if not pre_overheat_trigger and not overheat_trigger:
                     rospy.logwarn('Gripper motors are overheating [LM: %dC, RM: %dC]' % (l_temp, r_temp))
                     rospy.logwarn('Setting torque to [LM: %.2f, RM: %.2f]' % (l_goal, r_goal))
-                    self.left_finger_joint_position_pub.publish(l_goal)
-                    self.right_finger_joint_position_pub.publish(r_goal)
+                    self.left_finger_joint_torque_pub.publish(l_goal)
+                    self.right_finger_joint_torque_pub.publish(r_goal)
                     pre_overheat_trigger = True
             else:
                 pre_overheat_trigger = False
-                
-            if l_temp >= 66 or r_temp >= 66:
-                if not overheat_trigger:
-                    rospy.logwarn('Disabling gripper motors torque [LM: %dC, RM: %dC]' % (l_temp, r_temp))
-                    self.left_finger_joint_position_pub.publish(-0.5)
-                    self.right_finger_joint_position_pub.publish(0.5)
-                    overheat_trigger = True
-            else:
                 overheat_trigger = False
                 
+            self.overheating = pre_overheat_trigger or overheat_trigger
+            #########################################################################
+            
+            # don't do torque control if not requested or
+            # when the gripper is open
+            # or when motors are too hot
+            if not self.dynamic_torque_control or \
+               not self.close_gripper or \
+               self.overheating:
+                r.sleep()
+                continue
+                
+            #----------------------- TORQUE CONTROL -------------------------------#
+            l_current = self.left_finger_joint_state.goal_pos
+            r_current = self.right_finger_joint_state.goal_pos
+            
+            if pressure > self.upper_pressure:   # release
+                pressure_change_step = abs(pressure - self.upper_pressure) / max_pressure
+                left = min(DMXL_MAX_SPEED_RAD, l_current + pressure_change_step)
+                right = max(-DMXL_MAX_SPEED_RAD, r_current - pressure_change_step)
+                
+                if left < DMXL_MAX_SPEED_RAD or right > -DMXL_MAX_SPEED_RAD:
+                    self.left_finger_joint_torque_pub.publish(left)
+                    self.right_finger_joint_torque_pub.publish(right)
+                    rospy.logdebug('>MAX pressure is %.2f, LT: %.2f, RT: %.2f, step is %.2f' % (pressure, l_current, r_current, pressure_change_step))
+            elif pressure < self.lower_pressure: # squeeze
+                pressure_change_step = abs(pressure - self.lower_pressure) / max_pressure
+                left = max(-DMXL_MAX_SPEED_RAD, l_current - pressure_change_step)
+                right = min(DMXL_MAX_SPEED_RAD, r_current + pressure_change_step)
+                
+                if left > -DMXL_MAX_SPEED_RAD or right < DMXL_MAX_SPEED_RAD:
+                    self.left_finger_joint_torque_pub.publish(left)
+                    self.right_finger_joint_torque_pub.publish(right)
+                    rospy.logdebug('<MIN pressure is %.2f, LT: %.2f, RT: %.2f, step is %.2f' % (pressure, l_current, r_current, pressure_change_step))
+            ########################################################################
+            
             r.sleep()
+
 
     def process_left_finger_joint_state(self, msg):
         self.left_finger_joint_state = msg
 
+
     def process_right_finger_joint_state(self, msg):
         self.right_finger_joint_state = msg
 
+
     def process_pressure_sensors(self, msg, i):
         self.pressure[i] = msg.data
+
 
     def process_ir_sensor(self, msg):
         """
@@ -145,38 +198,59 @@ class GripperActionController():
         elif msg.data <= 530: self.ir_distance = 0.0        # too close
         else: self.ir_distance =  20.76 / (msg.data - 11)   # just right
 
+
     def process_gripper_action(self, req):
         r = rospy.Rate(500)
+        timeout = rospy.Duration(2.0)
         
         if req.command == WubbleGripperGoal.CLOSE_GRIPPER:
+            self.dynamic_torque_control = req.dynamic_torque_control
             desired_torque = req.torque_limit * DMXL_MAX_SPEED_RAD
-            self.left_finger_joint_position_pub.publish(-2.0)
-            self.right_finger_joint_position_pub.publish(2.0)
-            rospy.sleep(0.1)
+            self.left_finger_joint_torque_pub.publish(-desired_torque)
+            self.right_finger_joint_torque_pub.publish(desired_torque)
             
-            while self.left_finger_joint_state.is_moving or self.right_finger_joint_state.is_moving:
-                r.sleep()
+            self.lower_pressure = req.pressure_lower
+            self.upper_pressure = req.pressure_upper
+            
+            #while self.left_finger_joint_state.is_moving or self.right_finger_joint_state.is_moving:
+            if not self.dynamic_torque_control:
+                start_time = rospy.Time.now()
+                while (not self.within_tolerance(self.left_finger_joint_state.load, -req.torque_limit, 0.01) or
+                       not self.within_tolerance(self.right_finger_joint_state.load, -req.torque_limit, 0.01) or
+                       rospy.Time.now() - start_time < timeout) and \
+                      not rospy.is_shutdown():
+                    r.sleep()
+            else:
+                if desired_torque > 1e-3: rospy.sleep(1.0 / desired_torque)
                 
-            self.left_finger_joint_position_pub.publish(-desired_torque)
-            self.right_finger_joint_position_pub.publish(desired_torque)
+            self.close_gripper = True
             
             self.action_server.set_succeeded()
         elif req.command == WubbleGripperGoal.OPEN_GRIPPER:
+            self.close_gripper = False
             desired_torque = req.torque_limit * DMXL_MAX_SPEED_RAD
-            self.left_finger_joint_position_pub.publish(2.0)
-            self.right_finger_joint_position_pub.publish(-2.0)
+            self.left_finger_joint_torque_pub.publish(desired_torque)
+            self.right_finger_joint_torque_pub.publish(-desired_torque)
             
-            while self.left_finger_joint_state.current_pos < 0.8 or self.right_finger_joint_state.current_pos > -0.8:
+            start_time = rospy.Time.now()
+            while (self.left_finger_joint_state.current_pos < 0.8 or
+                   self.right_finger_joint_state.current_pos > -0.8 or
+                   rospy.Time.now() - start_time > timeout) and \
+                  not rospy.is_shutdown():
                 r.sleep()
                 
-            self.left_finger_joint_position_pub.publish(0.5)
-            self.right_finger_joint_position_pub.publish(0.5)
+            self.left_finger_joint_torque_pub.publish(0.5)
+            self.right_finger_joint_torque_pub.publish(-0.5)
             self.action_server.set_succeeded()
         else:
-            rospy.logerr('Unrecognized command, gripper is not moving enywhere!')
-            return
+            msg = 'Unrecognized command: %d' % req.command
+            rospy.logerr(msg)
+            self.action_server.set_aborted(text=msg)
+
 
 if __name__ == '__main__':
-    gac = GripperActionController()
-    rospy.spin()
+    try:
+        gac = GripperActionController()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
 
---
+++ trunk/arrg/ua_apps/wubble2_robot/scripts/push+newpick+shake+drop.py
@@ -0,0 +1,181 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2010, Antons Rebguns
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Willow Garage, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Antons Rebguns
+#
+
+PKG = 'wubble2_robot'
+NAME = 'all_actions'
+
+import roslib; roslib.load_manifest(PKG)
+import rospy
+
+from pr2_controllers_msgs.msg import JointTrajectoryAction
+from pr2_controllers_msgs.msg import JointTrajectoryGoal
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+from ua_audio_msgs.msg import TransformedStream
+
+from actionlib import SimpleActionClient
+from wubble2_robot.msg import *
+
+class ObjectSoundCollector:
+    def __init__(self):
+        self.sound = []
+        self.save_sound_flag = False
+        self.sound_sub = rospy.Subscriber('transform_audio', TransformedStream, self.process_audio)
+        
+        self.push_client = SimpleActionClient('wubble_arm_push_action', WubbleArmPushAction)
+        self.push_client.wait_for_server()
+        
+        self.gripper_client = SimpleActionClient('wubble_gripper_action', WubbleGripperAction)
+        self.gripper_client.wait_for_server()
+        
+        self.traj_client = SimpleActionClient('l_arm_controller/joint_trajectory_action', JointTrajectoryAction)
+        self.traj_client.wait_for_server()
+        
+        self.shake_client = SimpleActionClient('wubble_gripper_shake_action', WubbleGripperShakeAction)
+        self.shake_client.wait_for_server()
+
+
+    def process_audio(self, msg):
+        if self.save_sound: self.sound.extend(msg.stream)
+
+    def save_sound(self, action):
+        # save the sound to file and ditch it from memory
+        f = open('/tmp/%s_action_%f' % (action, rospy.Time.now().to_sec()), 'w')
+        f.write(str(self.sound))
+        f.close()
+
+    def close_gripper(self):
+        goal = WubbleGripperGoal()
+        goal.command = WubbleGripperGoal.CLOSE_GRIPPER
+        goal.torque_limit = 0.5
+        goal.dynamic_torque_control = False
+        goal.pressure_upper = 2100.0
+        goal.pressure_lower = 1900.0
+        
+        self.gripper_client.send_goal(goal)
+        self.gripper_client.wait_for_result()
+
+    def open_gripper(self):
+        goal = WubbleGripperGoal()
+        goal.command = WubbleGripperGoal.OPEN_GRIPPER
+        goal.torque_limit = 0.4
+        
+        self.gripper_client.send_goal(goal)
+        self.gripper_client.wait_for_result()
+
+    def lift(self):
+        goal = JointTrajectoryGoal()
+
+        # First, the joint names, which apply to all waypoints
+        goal.trajectory.joint_names.append('shoulder_pitch_joint')
+        goal.trajectory.joint_names.append('shoulder_yaw_joint')
+        goal.trajectory.joint_names.append('shoulder_roll_joint')
+        goal.trajectory.joint_names.append('elbow_pitch_joint')
+        goal.trajectory.joint_names.append('wrist_roll_joint')
+        goal.trajectory.joint_names.append('wrist_pitch_joint')
+        goal.trajectory.joint_names.append('wrist_yaw_joint')
+
+        point = JointTrajectoryPoint()
+        point.positions  = [-1.10, -1.58, 0.53, -0.76, -1.13, -0.31, -0.18]
+        point.velocities = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        point.time_from_start = rospy.Duration(3.0)
+        goal.trajectory.points.append(point)
+
+        goal.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.2)
+        self.traj_client.send_goal(goal)
+        self.traj_client.wait_for_result()
+
+    def push(self):
+        goal = WubbleArmPushGoal()
+        goal.side = WubbleArmPushGoal.RIGHT_PUSH
+        
+        self.save_sound_flag = True
+        
+        self.push_client.send_goal(goal)
+        self.push_client.wait_for_result()
+        
+        self.save_sound_flag = False
+        self.save_sound('push')
+        self.sound = []
+
+    def pick(self):
+        self.save_sound_flag = True
+        
+        self.close_gripper()
+        self.lift()
+        
+        self.save_sound_flag = False
+        self.save_sound('pick')
+        self.sound = []
+
+    def shake(self):
+        goal = WubbleGripperShakeGoal()
+        goal.shake_number = 5
+        
+        self.save_sound_flag = True
+        
+        self.shake_client.send_goal(goal)
+        self.shake_client.wait_for_result()
+        
+        self.save_sound_flag = False
+        self.save_sound('shake')
+        self.sound = []
+
+    def drop(self):
+        self.save_sound_flag = True
+        
+        self.open_gripper()
+        
+        rospy.sleep(0.5)
+        self.save_sound_flag = False
+        self.save_sound('drop')
+        self.sound = []
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node(NAME, anonymous=True)
+        osc = ObjectSoundCollector()
+        rospy.loginfo('Doing a Push action')
+        osc.push()
+        rospy.loginfo('Doing a PickUp action')
+        osc.pick()
+        rospy.loginfo('Doing a Shake action')
+        osc.shake()
+        rospy.loginfo('Doing a Drop action')
+        osc.drop()
+        
+        rospy.sleep(2)
+        rospy.loginfo('Closing gripper')
+        osc.close_gripper()
+    except rospy.ROSInterruptException:
+        pass
+
