124377ebb24cebfb09c91c91d0e79b5d30b426c7
deerslyr1 <deerslyr1@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-02-10 17:57:34 +0000

    Updated clustering to make more files rather than fewere.

--- trunk/arrg/ua_experimental/time_series/src/edu/arizona/cs/learn/timeseries/clustering/kmeans/KMeans.java
+++ trunk/arrg/ua_experimental/time_series/src/edu/arizona/cs/learn/timeseries/clustering/kmeans/KMeans.java
@@ -13,8 +13,10 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import edu.arizona.cs.learn.timeseries.clustering.ClusteringResults;
+import edu.arizona.cs.learn.timeseries.experiment.SyntheticExperiments;
 import edu.arizona.cs.learn.timeseries.model.Instance;
 import edu.arizona.cs.learn.timeseries.model.SequenceType;
+import edu.arizona.cs.learn.util.RandomFile;
 import edu.arizona.cs.learn.util.Utils;
 
 public class KMeans {
@@ -158,39 +160,175 @@ public class KMeans {
 	
 	public static void main(String[] args) throws Exception { 
 		// perform a test with a small subset of the total problem.
-		List<Instance> all = new ArrayList<Instance>();
-		List<Instance> set1 = Utils.sequences("A", "data/input/ww3d-jump-over.lisp", SequenceType.allen);
-		List<Instance> set2 = Utils.sequences("B", "data/input/ww3d-jump-on.lisp", SequenceType.allen);
-		List<Instance> set3 = Utils.sequences("C", "data/input/ww3d-left.lisp", SequenceType.allen);
-		List<Instance> set4 = Utils.sequences("D", "data/input/ww3d-right.lisp", SequenceType.allen);
-		List<Instance> set5 = Utils.sequences("E", "data/input/ww3d-push.lisp", SequenceType.allen);
-//		List<Instance> set6 = Utils.sequences("F", "data/input/ww3d-approach.lisp", SequenceType.allen);
-
-//		for (int i = 0; i < 10; ++i) { 
-//			all.add(set1.get(i));
-//			all.add(set2.get(i));
-//			all.add(set3.get(i));
-//			all.add(set4.get(i));
-//			all.add(set5.get(i));
-////			all.add(set6.get(i));
-//		}
+		if (args.length < 3) {
+			System.out.println("Usage: <prefix/synthetic> <ClusterType> <ClusterInit> ...");
+			System.out.println("  ClusterTypes: signature, medoid");
+			System.out.println("  ClusterInits: random, kPlusPlus, supervised1, supervised5, supervised10");
+			return;
+		}
+		String dataType = args[0];
 		
-		all.addAll(set1);
-		all.addAll(set2);
-		all.addAll(set3);
-		all.addAll(set4);
-		all.addAll(set5);
-//		all.addAll(set6);
+		List<ClusterType> types = new ArrayList<ClusterType>();
+		String[] typeTokens = args[1].split("[,]");
+		for (String tok : typeTokens)
+			types.add(ClusterType.valueOf(tok));
+		
+		List<ClusterInit> inits = new ArrayList<ClusterInit>();
+		String[] initTokens = args[2].split("[,]");
+		for (String tok : initTokens)
+			inits.add(ClusterInit.valueOf(tok));
 
-		PrintStream out = new PrintStream(new File("logs/synthetic-clustering-ww3d.csv"));
-		for (ClusterInit init : ClusterInit.values()) { 
-			for (ClusterType type : ClusterType.values()) { 
-				KMeans kmeans = new KMeans(5, 20, init, type);
-				kmeans.cluster(all);
+		Utils.EXPERIMENTS = Integer.parseInt(args[3]);
+		
+		if ("synthetic".equals(dataType)) { 
+			if (args.length != 8) {
+				System.out.println("Synthetic usage: synthetic <ClusterType> <ClusterInit> #Experiments " +
+						"#Repeats <Means> <Percents> <Lengths>");
+				System.out.println("  ClusterTypes: signature, medoid");
+				System.out.println("  ClusterInits: random, kPlusPlus, supervised1, supervised5, supervised10");
+				return;
+			}
+			
+			Utils.LIMIT_RELATIONS = true;
+			Utils.WINDOW = 2;
+			
+			int repeats = Integer.parseInt(args[4]);
+			
+			List<Double> means = new ArrayList<Double>();
+			String[] meanTokens = args[5].split("[,]");
+			for (String tok : meanTokens)
+				means.add(Double.parseDouble(tok));
+			
+			List<Double> pcts = new ArrayList<Double>();
+			String[] pctTokens = args[6].split("[,]");
+			for (String tok : pctTokens)
+				pcts.add(Double.parseDouble(tok));
+			
+			List<Integer> lengths = new ArrayList<Integer>();
+			String[] lengthTokens = args[7].split("[,]");
+			for (String tok : lengthTokens)
+				lengths.add(Integer.parseInt(tok));
+			
+			cluster(types, inits, repeats, lengths, means, pcts);
+		} else { 
+			if (args.length != 5) {
+				System.out.println("Other usage: dataType <ClusterType> <ClusterInit> #Experiments Directory");
+				System.out.println("  ClusterTypes: signature, medoid");
+				System.out.println("  ClusterInits: random, kPlusPlus, supervised1, supervised5, supervised10");
+				return;
+			}
+			
+			// We are dealing with deterministic data
+			// and therefore cannot generate random permutations
+			// of it.
+			String directory = args[4];
+			for (ClusterInit init : inits) { 
+				for (ClusterType type : types) { 
+					PrintStream out = new PrintStream(new File("logs/cluster-synthetic-" + 
+							type + "-" + init + "-" + dataType + ".csv"));
+					out.println("cluster_type,cluster_init,dataType,run,tp,fp,fn,tn,accuracy");
+					doCluster(out, directory, dataType, init, type, dataType);
+				}
 			}
 		}
+		
+//		List<Instance> all = new ArrayList<Instance>();
+//		List<Instance> set1 = Utils.sequences("A", "data/input/ww3d-jump-over.lisp", SequenceType.allen);
+//		List<Instance> set2 = Utils.sequences("B", "data/input/ww3d-jump-on.lisp", SequenceType.allen);
+//		List<Instance> set3 = Utils.sequences("C", "data/input/ww3d-left.lisp", SequenceType.allen);
+//		List<Instance> set4 = Utils.sequences("D", "data/input/ww3d-right.lisp", SequenceType.allen);
+//		List<Instance> set5 = Utils.sequences("E", "data/input/ww3d-push.lisp", SequenceType.allen);
+////		List<Instance> set6 = Utils.sequences("F", "data/input/ww3d-approach.lisp", SequenceType.allen);
+//
+////		for (int i = 0; i < 10; ++i) { 
+////			all.add(set1.get(i));
+////			all.add(set2.get(i));
+////			all.add(set3.get(i));
+////			all.add(set4.get(i));
+////			all.add(set5.get(i));
+//////			all.add(set6.get(i));
+////		}
+//		
+//		all.addAll(set1);
+//		all.addAll(set2);
+//		all.addAll(set3);
+//		all.addAll(set4);
+//		all.addAll(set5);
+////		all.addAll(set6);
+//
+//		PrintStream out = new PrintStream(new File("logs/synthetic-clustering-ww3d.csv"));
+//		for (ClusterInit init : ClusterInit.values()) { 
+//			for (ClusterType type : ClusterType.values()) { 
+//				KMeans kmeans = new KMeans(5, 20, init, type);
+//				kmeans.cluster(all);
+//			}
+//		}
 	}
 
+
+	/**
+	 * 
+	 * @param types
+	 * @param inits
+	 * @param repeats
+	 * @param episodeLengths
+	 * @param means
+	 * @param pcts
+	 * @throws Exception
+	 */
+	public static void cluster(List<ClusterType> types, List<ClusterInit> inits, int repeats,
+			List<Integer> episodeLengths, List<Double> means, List<Double> pcts) throws Exception { 
+		String pid = RandomFile.getPID();
+
+		for (double pct : pcts) { 
+			for (double mean : means) { 
+				for (int length : episodeLengths) { 
+					for (int i = 0; i < repeats; ++i) { 
+						SyntheticExperiments.generateClass(pid, "f", 0, 0, length);
+						SyntheticExperiments.generateClass(pid, "g", mean, pct, length);
+
+						String params = pct + "-" + mean + "-" + length;
+						String rowString = pct + "," + mean + "," + length;
+						for (ClusterInit init : inits) { 
+							for (ClusterType type : types) { 
+								PrintStream out = new PrintStream(new File("logs/cluster-synthetic-" + 
+										type + "-" + init + "-" + params + ".csv"));
+								out.println("cluster_type,cluster_init,pct,mean,length,run,tp,fp,fn,tn,accuracy");
+								doCluster(out, "/tmp/niall-" + pid + "/", "niall", init, type, rowString);
+							}
+						}
+					}
+				}
+			}
+		}
+	}	
+	
+	/**
+	 * Run some number of clusters through and output the results.
+	 * @param out
+	 * @param dir
+	 * @param prefix
+	 * @param init
+	 * @param type
+	 * @param rowInfo
+	 */
+	public static void doCluster(PrintStream out, String dir, String prefix, 
+			ClusterInit init, ClusterType type, String rowInfo) { 
+		Map<String,List<Instance>> data = Utils.load(dir, prefix, SequenceType.allen);
+		List<Instance> all = new ArrayList<Instance>();
+		for (String name : data.keySet()) { 
+			all.addAll(data.get(name));
+		}
+
+		// now we repeat the experiment x number of times....
+		for (int i = 0; i < Utils.EXPERIMENTS; ++i) { 
+			KMeans kmeans = new KMeans(data.keySet().size(), 20, init, type);
+			ClusteringResults result = kmeans.cluster(all);
+			out.println(type + "," + init + "," + rowInfo + "," + i + 
+					"," + result.tp + "," + result.fp + "," + result.fn + "," + result.tn + 
+					"," + result.accuracy);
+		}
+	}
 	
 }
 
