0655a4f8e010e5a7d7a184501bee2b9fecfd98b3
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-03-04 00:56:08 +0000

    Reenable trajectory controller

--- arrg/ua_apps/wubble2_robot/scripts/push+newpick+shake+drop.py
+++ arrg/ua_apps/wubble2_robot/scripts/push+newpick+shake+drop.py
@@ -97,12 +97,12 @@ class ObjectSoundCollector:
 
         # First, the joint names, which apply to all waypoints
         goal.trajectory.joint_names.append('shoulder_pitch_joint')
-        goal.trajectory.joint_names.append('shoulder_yaw_joint')
-        goal.trajectory.joint_names.append('shoulder_roll_joint')
-        goal.trajectory.joint_names.append('elbow_pitch_joint')
-        goal.trajectory.joint_names.append('wrist_roll_joint')
+        goal.trajectory.joint_names.append('shoulder_pan_joint')
+        goal.trajectory.joint_names.append('upperarm_roll_joint')
+        goal.trajectory.joint_names.append('elbow_flex_joint')
+        goal.trajectory.joint_names.append('forearm_roll_joint')
         goal.trajectory.joint_names.append('wrist_pitch_joint')
-        goal.trajectory.joint_names.append('wrist_yaw_joint')
+        goal.trajectory.joint_names.append('wrist_roll_joint')
 
         point = JointTrajectoryPoint()
         point.positions  = [-1.10, -1.58, 0.53, -0.76, -1.13, -0.31, -0.18]
@@ -127,8 +127,8 @@ class ObjectSoundCollector:
         self.save_sound('push')
         self.sound = []
         
-        result = self.push_client.get_result()
-        return result.success
+        #result = self.push_client.get_result()
+        #return result.success
 
 
     def pick(self):
@@ -168,20 +168,27 @@ if __name__ == '__main__':
     try:
         rospy.init_node(NAME, anonymous=True)
         osc = ObjectSoundCollector()
-        rospy.loginfo('Doing a Push action')
-        if not osc.push(): exit(1)
-        
-        rospy.loginfo('Doing a PickUp action')
-        osc.pick()
-        rospy.loginfo('Doing a Shake action')
-        osc.shake()
-        rospy.loginfo('Doing a Drop action')
-        osc.drop()
-        
-        rospy.sleep(2)
+
         rospy.loginfo('Closing gripper')
         osc.close_gripper(0.2, False)
         osc.close_gripper(0.05, False)
+        rospy.sleep(2)
+
+        rospy.loginfo('Doing a Push action')
+        osc.push()
+        #if not osc.push(): exit(1)
+        
+#        rospy.loginfo('Doing a PickUp action')
+#        osc.pick()
+#        rospy.loginfo('Doing a Shake action')
+#        osc.shake()
+#        rospy.loginfo('Doing a Drop action')
+#        osc.drop()
+#        
+#        rospy.sleep(2)
+#        rospy.loginfo('Closing gripper')
+#        osc.close_gripper(0.2, False)
+#        osc.close_gripper(0.05, False)
     except rospy.ROSInterruptException:
         pass
 
