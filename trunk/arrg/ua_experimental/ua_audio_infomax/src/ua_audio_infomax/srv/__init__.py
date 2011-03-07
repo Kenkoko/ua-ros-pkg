15b87990413502b74ac7a04486a24105dfd830c7
daniel.jb.ford@gmail.com <daniel.jb.ford@gmail.com@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-03-07 17:42:45 +0000

    Cleaned out all autogenerated files

--- trunk/arrg/ua_experimental/ua_audio_infomax/src/ua_audio_infomax/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from _InfoMax import *
--- trunk/arrg/ua_experimental/ua_audio_infomax/srv/lisp/ua_audio_infomax/_package_InfoMax.lisp
+++
@@ -1,7 +0,0 @@
-(in-package ua_audio_infomax-srv)
-(export '(OBJECTNAMES-VAL
-          ACTIONNAMES-VAL
-          ACTIONID-VAL
-          BELIEFS-VAL
-          LOCATION-VAL
-))
