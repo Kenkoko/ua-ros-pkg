bfb2ebb1c5b5d5b1ecc1eaa7f5aeee4654891da8
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-03-13 00:02:22 +0000

    updated version of audio recording node

--- arrg/ua_experimental/ua_audio_capture/audio_dump.py
+++ arrg/ua_experimental/ua_audio_capture/audio_dump.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+# Author: Antons Rebguns
+
 import roslib; roslib.load_manifest('ua_audio_capture')
 from ua_audio_msgs.msg import AudioRawStream
 
 import rospy
 from threading import Lock
-from std_srvs.srv import Empty
+from ua_audio_capture.srv import StartAudioRecording
+from ua_audio_capture.srv import StopAudioRecording
+from ua_audio_capture.srv import StopAudioRecordingResponse
 
 from array import array
 
@@ -13,7 +17,7 @@ class AudioDump():
     def __init__(self):
         self.save_audio = False
         self.stream = []
-        self.counter = 0
+        self.counter = 1
         self.object_id = 0
         self.action_id = 0
         self.last_index = 0
@@ -26,47 +30,65 @@ class AudioDump():
         self.fdesc = open('/tmp/sound_%d.desc' % time, 'w')
         
         rospy.Subscriber('audio_capture/audio', AudioRawStream, self.process_audio)
-        rospy.Service('audio_dump/trigger', Empty, self.process_trigger)
+        rospy.Service('audio_dump/start_audio_recording', StartAudioRecording, self.process_start_recording)
+        rospy.Service('audio_dump/stop_audio_recording', StopAudioRecording, self.process_stop_recording)
 
     def __del__(self):
         self.f.close()
         self.fdesc.close()
 
     def process_audio(self, msg):
-        self.l.acquire()
         if self.save_audio:
+            self.l.acquire()
             self.stream.extend(msg.samples)
-        self.l.release()
+            self.l.release()
 
-    def process_trigger(self, req):
-        rospy.loginfo('Trigger #%d, save audio currently is %s, will set to %s' % (self.counter, str(self.save_audio), str(not self.save_audio)))
+    def process_start_recording(self, req):
+        rospy.loginfo('Recording sound #%d' % self.counter)
         
         self.l.acquire()
-        self.save_audio = not self.save_audio
+        self.action_id = req.action_id
+        self.object_id = req.object_id
+        self.save_audio = True
+        self.l.release()
+        
+        return [] # success
+
+    def process_stop_recording(self, req):
+        rospy.loginfo('Stopping recording of sound sample #%d' % self.counter)
+        res = StopAudioRecordingResponse()
         
         # was recording, now stop and dump to file
-        if not self.save_audio:
+        if self.save_audio:
             # save the sound to file and ditch it from memory
-            self.save_sound()
-            self.stream = []
+            self.save_audio = False
             
-        self.l.release()
-        
+            self.l.acquire()
+            res.recorded_sound = self.stream
+            self.stream = []
+            self.l.release()
+            
+            if req.dump_to_file:
+                rospy.loginfo('Save current sound sample to file')
+                self.save_sound(res.recorded_sound)
+            else:
+                rospy.logwarn('Will not save current sound sample to file')
+        else:
+            rospy.logwarn('Was not recording audio, will return empty array')
+            
         self.counter += 1
-        return []   # success
+        return res
 
-    def save_sound(self):
-        stream_array = array('d', self.stream)
+    def save_sound(self, sound):
+        stream_array = array('d', sound)
         stream_array.tofile(self.f)
-        print 'current stream has %d numbers' % len(self.stream)
+        rospy.loginfo('current stream has %d numbers' % len(sound))
         offset = self.last_index
-        self.last_index += len(self.stream)
-        print 'current sound begins at %d and next sound will start at %d' % (offset, self.last_index)
-        desc = '%d %d %d %d\n' % (self.object_id, self.action_id, offset, len(self.stream))
+        self.last_index += len(sound)
+        rospy.loginfo('current sound begins at %d and next sound will start at %d' % (offset, self.last_index))
+        desc = '%d %d %d %d\n' % (self.action_id, self.object_id, offset, len(sound))
         self.fdesc.write(desc)
-        
-        #self.object_id += 1
-        #self.f.write(str(self.stream))
+        self.fdesc.flush()
 
 if __name__ == '__main__':
     a = AudioDump()
