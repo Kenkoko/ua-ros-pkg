85d9dd69e9b82194a732bbda41f3104e3dcd5975
mohsen.malmir@gmail.com <mohsen.malmir@gmail.com@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-11-23 16:56:41 +0000

    shot descriptor classifier

---
+++ arrg/ua_experimental/point_cloud_classifier/src/shot_extractor.cpp
@@ -0,0 +1,70 @@
+#include "ros/ros.h"
+#include "point_cloud_classifier/extractShot.h"
+#include "sensor_msgs/PointCloud.h"
+#include "sensor_msgs/point_cloud_conversion.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/shot.h>
+#include <pcl/features/normal_3d.h>    
+#include <pcl/octree/octree.h>
+#include <pcl/ros/conversions.h>
+
+using namespace pcl;
+
+bool extract(point_cloud_classifier::extractShot::Request  &req,
+         point_cloud_classifier::extractShot::Response &res )
+{
+	float		kernelSize;
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudIn (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::SHOTEstimation<pcl::PointXYZ,pcl::Normal,pcl::SHOT> shotExtractor;
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+	pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr tree (new pcl::KdTreeFLANN<pcl::PointXYZ> ());
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+	pcl::PointCloud<pcl::SHOT>::Ptr cloudShot (new pcl::PointCloud<pcl::SHOT> ());
+
+	//initialization
+	kernelSize = 5;
+	sensor_msgs::PointCloud2 pcl2;
+	sensor_msgs:convertPointCloudToPointCloud2(req.point_cloud,pcl2);
+	fromROSMsg(pcl2,*cloudIn);
+	//estimating input normals
+	ne.setInputCloud (cloudIn);
+	ne.setSearchMethod (tree);
+	ne.setRadiusSearch (0.03);// Use all neighbors in a sphere of radius 3cm
+	ne.compute (*cloud_normals);
+	//extract shot features for the input cloud
+	shotExtractor.setInputCloud (cloudIn);
+	shotExtractor.setInputNormals(cloud_normals);
+	shotExtractor.setRadiusSearch(kernelSize);
+	shotExtractor.compute(*cloudShot);
+	//response
+	res.num_features = cloudShot->size();
+	res.feature_size = cloudShot->points[0].descriptor.size();
+	for(int i=0;i<cloudShot->size();i++)
+	{
+		for(int j=0;j<cloudShot->points[i].descriptor.size();j++)
+		{
+			res.shot_feature.push_back(cloudShot->points[i].descriptor[j]);
+		}
+	}
+
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "shotExtractor");
+	ros::NodeHandle n;
+	ros::ServiceServer service = n.advertiseService("extractShot", extract);
+	ROS_INFO("Ready to extract shot features.");
+	ros::spin();
+	return 0;
+}
+
+
