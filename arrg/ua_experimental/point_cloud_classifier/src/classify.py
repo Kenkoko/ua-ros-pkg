52bf337ba51e3aba798da07cdf3d6a8bd28c9d90
arebgun <arebgun@2d0f1ff0-de04-11de-a762-f1e63c3c121e>
2011-11-29 01:01:20 +0000

    move data files to data directory and make path configurable from launch file

--- arrg/ua_experimental/point_cloud_classifier/src/classify.py
+++ arrg/ua_experimental/point_cloud_classifier/src/classify.py
@@ -7,97 +7,109 @@ from point_cloud_classifier.srv import GetClusterLabelsResponse
 from point_cloud_classifier.srv import ExtractSHOTDescriptor
 from sklearn import decomposition,neighbors
 from numpy import *
+import os
 
 
 class ClusterClassification:
     def __init__(self):
-        #init
-        nComponents = 10#PCA components
-        nCodeWords  = 200#knn codewords
+        self.index_to_label = {
+            0: 'Lego',
+            1: 'Box',
+            2: 'Toy',
+            3: 'Ball',
+            4: 'Cup',
+        }
+        
+        # PCA components
+        num_components = rospy.get_param('num_components', 10)
+        
+        # knn codewords
+        num_code_words  = rospy.get_param('num_code_words', 200)
+        
+        # path to directory where all files are stored
+        default_data_path = os.path.join(roslib.packages.get_pkg_dir('point_cloud_classifier'), 'data')
+        data_path = rospy.get_param('data_path', default_data_path)
+        
         rospy.loginfo('Loading codebook...')
-        self.codeBook = loadtxt('codebook.txt')
-        #PCA
+        self.code_book = loadtxt(os.path.join(data_path, 'codebook.txt'))
+        
+        # PCA
         rospy.loginfo('Loading PCA data...')
-	self.pcaMean  = loadtxt('pcamean.txt');
-	self.pcaComps = loadtxt('pcacomps.txt');
-	self.mean     = loadtxt('mean.txt');
-	self.std      = loadtxt('std.txt');
-	
+        self.pca_mean  = loadtxt(os.path.join(data_path, 'pcamean.txt'))
+        self.pca_comps = loadtxt(os.path.join(data_path, 'pcacomps.txt'))
+        self.mean      = loadtxt(os.path.join(data_path, 'mean.txt'))
+        self.std       = loadtxt(os.path.join(data_path, 'std.txt'))
+        
 #        data = loadtxt('shot.txt')
 #        mean = data.mean(axis=0)
 #        std = sqrt(data.var(axis=0))
 #        std [where(std==0)] = 1
 #        data = (data - mean ) / std
 #        rospy.loginfo('Calculating PCA')
-#        pca = decomposition.PCA(n_components=10)
+#        pca = decomposition.PCA(n_components=num_components)
 #        pca.fit(data)
 #        del data
 #        self.mean = mean
 #        self.std  = std
 #        self.pca  = pca
-        #KNN
+        
+        # KNN
         rospy.loginfo('Loading training data...')
-        trainData = loadtxt('dataTrain.txt')
+        trainData = loadtxt(os.path.join(data_path, 'dataTrain.txt'))
         rospy.loginfo('Training KNN...')
-        knn = neighbors.NeighborsClassifier(1)
-        knn.fit(trainData[:,:-1], trainData[:,-1])
-        self.knn = knn
+        self.knn = neighbors.NeighborsClassifier(1)
+        self.knn.fit(trainData[:,:-1], trainData[:,-1])
         del trainData
         
-        #services
-        self.s = rospy.Service('get_cluster_labels', GetClusterLabels, self.get_cluster_labels)
-        
         rospy.wait_for_service('extract_shot_descriptor')
-        self.shotExtractor = rospy.ServiceProxy('extract_shot_descriptor', ExtractSHOTDescriptor)
+        self.extract_shot_features_srv = rospy.ServiceProxy('extract_shot_descriptor', ExtractSHOTDescriptor)
         rospy.loginfo('Classification node initialization done')
         
-    def get_cluster_labels(self,req):
+        rospy.Service('get_cluster_labels', GetClusterLabels, self.get_cluster_labels)
+
+    def get_cluster_labels(self, req):
         rospy.loginfo('got %d clusters' % len(req.clusters))
         res = GetClusterLabelsResponse()
         clusters = req.clusters
-        for idx,c in enumerate(clusters):
-            num_features, feature_size, shotFeatures = self.ExtractSHOTDescriptorFeatures(c)
-            label = self.Classify(num_features, feature_size, shotFeatures)
-            res.labels.append(label)
+        
+        for idx,point_cloud in enumerate(clusters):
+            try:
+                label = self.classify(point_cloud)
+                res.labels.append(label)
+            except Exception as e:
+                label = 'Unknown'
+                rospy.logerr('classification failed: %s' % e)
+                
             rospy.loginfo('labeled object %d with %s category' % (idx, label))
+            
         return res
 
-    def Classify(self, num_features, feature_size, shotFeatures):
-        shotFeatures = reshape(shotFeatures,(num_features, feature_size))
-        shotFeatures = shotFeatures - self.mean
-        shotFeatures = shotFeatures / self.std
-	#pca transform
-        temp = shotFeatures - self.pcaMean
-        objectPca = dot(temp, self.pcaComps.T)
-#        objectPca = self.pca.transform(shotFeatures)
-        objectHist = zeros((self.codeBook.shape[0]))
+    def classify(self, point_cloud):
+        res = self.extract_shot_features_srv(point_cloud)
+        num_features = res.num_features
+        feature_size = res.feature_size
+        shot_features = res.shot_feature
+        rospy.loginfo('successfully extarcted shot features')
+        
+        shot_features = reshape(shot_features, (num_features, feature_size))
+        shot_features = shot_features - self.mean
+        shot_features = shot_features / self.std
+        
+        #pca transform
+        temp = shot_features - self.pca_mean
+        objectPca = dot(temp, self.pca_comps.T)
+#        objectPca = self.pca.transform(shot_features)
+        objectHist = zeros((self.code_book.shape[0]))
+        
         for i in range(objectPca.shape[0]):
-            dist = sum( (self.codeBook - objectPca[i,:])**2 , axis=1 )
+            dist = sum( (self.code_book - objectPca[i,:])**2 , axis=1 )
             objectHist[argmin(dist)] = objectHist[argmin(dist)] + 1
+            
         objectHist = objectHist / objectHist.sum()
-        idx = self.knn.predict(objectHist)
-        return self.IdxToLabel(idx)
-
-    def IdxToLabel(self,idx):
-        if idx==0:
-            return 'Lego'
-        elif idx==1:
-            return 'Box'
-        elif idx==2:
-            return 'Toy'
-        elif idx==3:
-            return 'Ball'
-        elif idx==4:
-            return 'Cup'
-        else:
-            return 'Unknown'
-    
-    def ExtractSHOTDescriptorFeatures(self,point_cloud):
-        try:
-            resp1 = self.shotExtractor(point_cloud)
-            return resp1.num_features, resp1.feature_size, resp1.shot_feature
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
+        idx = self.knn.predict(objectHist)[0]
+        
+        if idx in self.index_to_label: return self.index_to_label[idx]
+        else: return 'Unknown'
 
 
 if __name__ == '__main__':
